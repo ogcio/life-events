682f314a13bf66041b900db027a115154e2ef726
Luke Jones <luke.jones@nearform.com>
2024-03-04 15:01:00 +0000

    chore: clean up old payment files (#29)
    * chore: clean up old payment files
    
    * chore: clean up translations

--- apps/web/app/[locale]/[event]/[...action]/page.tsx
+++ apps/web/app/[locale]/[event]/[...action]/page.tsx
@@ -2,10 +2,6 @@ import { routeDefinitions } from "../../../routeDefinitions";
 
 import RenewDriversLicence from "./RenewDriversLicence/RenewDriversLicence";
 import { NextPageProps } from "./types";
-import PayPaymentRequest from "./PayPaymentRequest/Pay";
-import PayBankPaymentRequest from "./PayPaymentRequest/Bank";
-import PaymentComplete from "./PayPaymentRequest/Complete";
-import PaymentError from './PayPaymentRequest/Error';
 
 export default function ActionPage(props: NextPageProps) {
   switch (props.params.action.at(0)) {
@@ -14,36 +10,6 @@ export default function ActionPage(props: NextPageProps) {
         <RenewDriversLicence
           params={props.params}
           searchParams={props.searchParams}
-        />
-      );
-    case routeDefinitions.paymentRequest.pay.slug:
-      if (props.params.action.length > 1) {
-        const paymentType = props.params.action.at(1);
-        if (paymentType === "bank") {
-          return (
-            <PayBankPaymentRequest
-              searchParams={
-                props.searchParams as { paymentId: string } | undefined
-              }
-            />
-          );
-        }
-      }
-      return (
-        <PayPaymentRequest
-          searchParams={props.searchParams as { paymentId: string } | undefined}
-        />
-      );
-    case routeDefinitions.paymentRequest.complete.slug:
-      return (
-        <PaymentComplete
-          searchParams={props.searchParams as { payment_id: string }}
-        />
-      );
-    case routeDefinitions.paymentRequest.error.slug:
-      return (
-        <PaymentError
-          searchParams={props.searchParams as { transactionId: string, status: string }}
         />
       );
   }
