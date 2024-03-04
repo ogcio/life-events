00664e94da1a58c3a70cc4e8f1b3263dfc57b361
William Monteiro <williamwd@users.noreply.github.com>
2024-03-04 07:11:32 -0300

    Adding localstack to enable use of S3 API (#17)

---
+++ init-localstack.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Show output for all main resources created
+set -x
+
+# Create S3 buckets
+awslocal s3 mb s3://$FILES_S3_BUCKET
+awslocal s3 mb s3://$IMAGES_S3_BUCKET
+
+# Set CORS for S3 buckets
+echo '{
+  "CORSRules": [
+    {
+      "AllowedHeaders": ["*"],
+      "AllowedMethods": ["GET", "POST", "PUT", "HEAD", "DELETE"],
+      "AllowedOrigins": [
+        "http://mock-api",
+        "http://web",
+        "https://app.localstack.cloud",
+        "http://app.localstack.cloud"
+      ],
+      "ExposeHeaders": ["ETag"]
+    }
+  ]
+}' > cors-config.json
+
+awslocal s3api put-bucket-cors --bucket "$IMAGES_S3_BUCKET" --cors-configuration file://cors-config.json
+
+set +x 
