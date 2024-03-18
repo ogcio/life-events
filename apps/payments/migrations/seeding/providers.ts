851ada250d29233c18dc1f8f63d9404ee42ed127
Alfonso Graziano <alfonso.graziano@nearform.com>
2024-03-18 09:26:22 +0100

    include ob

--- apps/payments/migrations/seeding/providers.ts
+++ apps/payments/migrations/seeding/providers.ts
@@ -1,7 +1,7 @@
 export const seedProviders = (pool, userId) => {
   console.log("Seeding providers");
-  //Manual Bank Transfer
-  return pool.query(
+
+  const manualBankTransfer = pool.query(
     `INSERT INTO payment_providers(user_id, provider_name, provider_type, status, provider_data) 
      VALUES($1, $2, $3, $4, $5)`,
     [
@@ -16,4 +16,22 @@ export const seedProviders = (pool, userId) => {
       }),
     ],
   );
+
+  const openBanking = pool.query(
+    `INSERT INTO payment_providers(user_id, provider_name, provider_type, status, provider_data) 
+     VALUES($1, $2, $3, $4, $5)`,
+    [
+      userId,
+      "Open Banking provider",
+      "openbanking",
+      "connected",
+      JSON.stringify({
+        sortCode: "123456",
+        accountNumber: "12345678",
+        accountHolderName: "Lorem Ipsum (OB)",
+      }),
+    ],
+  );
+
+  return Promise.all([manualBankTransfer, openBanking]);
 };
