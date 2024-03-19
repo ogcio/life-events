8dc8b334a5ed610c3b8a76f044902b6cfbfc15ad
Alfonso Graziano <alfonso.graziano@nearform.com>
2024-03-19 12:04:16 +0100

    refactor: extract paymentSetupForm

---
+++ apps/payments/app/[locale]/(hosted)/paymentSetup/PaymentSetupForm.tsx
@@ -0,0 +1,187 @@
+import { getTranslations } from "next-intl/server";
+import { pgpool } from "../../../dbConnection";
+
+async function getRegisteredAccounts(userId: string, providerType: string) {
+  "use server";
+
+  const accountsQueryResult = await pgpool.query<
+    { provider_id: string; provider_name: string },
+    string[]
+  >(
+    `select provider_id, provider_name from payment_providers where user_id = $1 and provider_type = $2`,
+    [userId, providerType],
+  );
+
+  if (!accountsQueryResult.rowCount) {
+    return [];
+  }
+
+  return accountsQueryResult.rows;
+}
+
+type PaymentSetupFormProps = {
+  requestId?: string;
+  userId: string;
+  action: (formData: FormData) => void;
+};
+
+export default async function ({
+  requestId,
+  userId,
+  action,
+}: PaymentSetupFormProps) {
+  const t = await getTranslations("PaymentSetup.CreatePayment");
+  const tCommon = await getTranslations("Common");
+
+  const [openBankingAccounts, stripeAccounts, manualBankTransferAccounts] =
+    await Promise.all([
+      getRegisteredAccounts(userId, "openbanking"),
+      getRegisteredAccounts(userId, "stripe"),
+      getRegisteredAccounts(userId, "banktransfer"),
+    ]);
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column", flex: 1 }}>
+      <form style={{ maxWidth: "500px" }} action={action}>
+        <h1 className="govie-heading-l">{t("title")}</h1>
+        <div className="govie-form-group">
+          <label htmlFor="title" className="govie-label--s">
+            {t("form.title")}
+          </label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            className="govie-input"
+            required
+          />
+        </div>
+        <div className="govie-form-group">
+          <label htmlFor="description" className="govie-label--s">
+            {t("form.description")}
+          </label>
+          <textarea
+            id="description"
+            name="description"
+            className="govie-textarea"
+            rows={5}
+          ></textarea>
+        </div>
+        <div className="govie-form-group">
+          <label htmlFor="openbanking-account" className="govie-label--s">
+            {t("form.paymentProvider.openbanking")}
+          </label>
+          <select
+            id="openbanking-account"
+            name="openbanking-account"
+            className="govie-select"
+          >
+            <option value={""}>Disabled</option>
+            {openBankingAccounts.map((account) => (
+              <option key={account.provider_id} value={account.provider_id}>
+                {account.provider_name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="govie-form-group">
+          <label htmlFor="stripe-account" className="govie-label--s">
+            {t("form.paymentProvider.stripe")}
+          </label>
+          <select
+            id="stripe-account"
+            name="stripe-account"
+            className="govie-select"
+          >
+            <option value={""}>Disabled</option>
+            {stripeAccounts.map((account) => (
+              <option key={account.provider_id} value={account.provider_id}>
+                {account.provider_name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="govie-form-group">
+          <label htmlFor="banktransfer-account" className="govie-label--s">
+            {t("form.paymentProvider.banktransfer")}
+          </label>
+          <select
+            id="banktransfer-account"
+            name="banktransfer-account"
+            className="govie-select"
+          >
+            <option value={""}>Disabled</option>
+            {manualBankTransferAccounts.map((account) => (
+              <option key={account.provider_id} value={account.provider_id}>
+                {account.provider_name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="govie-form-group">
+          <label htmlFor="reference" className="govie-label--s">
+            {t("form.reference")}
+          </label>
+          <input
+            type="text"
+            id="reference"
+            name="reference"
+            className="govie-input"
+            required
+          />
+        </div>
+        <div className="govie-form-group">
+          <label htmlFor="amount" className="govie-label--s">
+            {t("form.amount")}
+          </label>
+          <div className="govie-input__wrapper">
+            <div aria-hidden="true" className="govie-input__prefix">
+              {t("form.currencySymbol")}
+            </div>
+            <input
+              type="number"
+              id="amount"
+              name="amount"
+              className="govie-input"
+              min="0.00"
+              max="10000.00"
+              step="0.01"
+              required
+            />
+          </div>
+        </div>
+        <div className="govie-form-group">
+          <div className="govie-checkboxes__item">
+            <input
+              className="govie-checkboxes__input"
+              id="allow-override-hint"
+              name="allowAmountOverride"
+              type="checkbox"
+            />
+            <label
+              className="govie-label--s govie-checkboxes__label"
+              htmlFor="allow-override-hint"
+            >
+              {t("form.allowAmountOverride")}
+            </label>
+          </div>
+        </div>
+        <div className="govie-form-group">
+          <label htmlFor="redirect-url" className="govie-label--s">
+            {t("form.redirectUrl")}
+          </label>
+          <input
+            type="url"
+            id="reference"
+            name="redirect-url"
+            className="govie-input"
+            required
+          />
+        </div>
+
+        <input type="submit" value={tCommon("save")} className="govie-button" />
+      </form>
+    </div>
+  );
+}
