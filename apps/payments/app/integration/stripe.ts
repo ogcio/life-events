5dfaa5caa4634fe613c1aed8dfca4778e4def2ef
Alfonso Graziano <alfonso.graziano@nearform.com>
2024-03-22 10:13:53 +0100

    Fix (#34)

--- apps/payments/app/integration/stripe.ts
+++ apps/payments/app/integration/stripe.ts
@@ -1,17 +1,19 @@
 import s from "stripe";
 
-const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
-if (!STRIPE_SECRET_KEY) throw new Error("Stripe secret key not found");
+const getStripeInstance = () => {
+  const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
+  if (!STRIPE_SECRET_KEY) throw new Error("Stripe secret key not found");
 
-const stripe = new s(STRIPE_SECRET_KEY);
+  return new s(STRIPE_SECRET_KEY);
+};
 
 export async function createPaymentIntent(paymentRequest) {
-  return await stripe.paymentIntents.create({
+  return await getStripeInstance().paymentIntents.create({
     amount: paymentRequest.amount,
     currency: "GBP",
   });
 }
 
 export async function getPaymentIntent(clientSecret) {
-  return await stripe.paymentIntents.retrieve(clientSecret);
+  return await getStripeInstance().paymentIntents.retrieve(clientSecret);
 }
