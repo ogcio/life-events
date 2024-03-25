ed6adbd8c5cc8cc5fcb24c7ef36e9d25423eb560
Sara Zanellato <47299026+peschina@users.noreply.github.com>
2024-03-25 11:50:23 +0100

    authenticate once per worker (#42)

--- apps/payments/playwright.config.ts
+++ apps/payments/playwright.config.ts
@@ -18,48 +18,34 @@ export default defineConfig({
     : [["list"], ["html", { outputFolder: "./playwright/playwright-report" }]],
   projects: [
     {
-      name: "setup auth",
-      testMatch: "auth.ts",
-    },
-    {
       name: "Desktop Chrome",
       use: {
         ...devices["Desktop Chrome"],
-        storageState: "e2e/.auth/user.json",
       },
-      dependencies: ["setup auth"],
     },
-    // {
-    //   name: 'Desktop Firefox',
-    //   use: {
-    //     ...devices['Desktop Firefox'],
-    //     storageState: 'e2e/.auth/user.json',
-    //   },
-    //   dependencies: ['setup auth'],
-    // },
-    // {
-    //   name: 'Desktop Safari',
-    //   use: {
-    //     ...devices['Desktop Safari'],
-    //     storageState: 'e2e/.auth/user.json',
-    //   },
-    //   dependencies: ['setup auth'],
-    // },
-    // {
-    //   name: "Mobile Chrome",
-    //   use: {
-    //     ...devices["Pixel 5"],
-    //     storageState: 'e2e/.auth/user.json',
-    //   },
-    //   dependencies: ['setup auth'],
-    // },
-    // {
-    //   name: "Mobile Safari",
-    //   use: {
-    //     ...devices["iPhone 12"],
-    //     storageState: 'e2e/.auth/user.json',
-    //   },
-    //   dependencies: ['setup auth'],
-    // },
+    {
+      name: "Desktop Firefox",
+      use: {
+        ...devices["Desktop Firefox"],
+      },
+    },
+    {
+      name: "Desktop Safari",
+      use: {
+        ...devices["Desktop Safari"],
+      },
+    },
+    {
+      name: "Mobile Chrome",
+      use: {
+        ...devices["Pixel 5"],
+      },
+    },
+    {
+      name: "Mobile Safari",
+      use: {
+        ...devices["iPhone 12"],
+      },
+    },
   ],
 });
