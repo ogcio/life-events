d463d9ec3ba2a36aa4a97a04685be61826b24de0
Sara Zanellato <sara.zanellato@nearform.com>
2024-03-25 11:07:15 +0100

    authenticate once per worker

--- apps/payments/e2e/providers/providers.spec.ts
+++ apps/payments/e2e/providers/providers.spec.ts
@@ -1,4 +1,5 @@
-import { test } from "@playwright/test";
+import { expect } from "@playwright/test";
+import { test } from "../fixtures/test";
 
 test("should be able to add a bank transfer provider", async ({ page }) => {
   await page.goto("/en/paymentSetup");
@@ -8,14 +9,11 @@ test("should be able to add a bank transfer provider", async ({ page }) => {
   await page
     .getByRole("button", { name: "Select Manual Bank Transfer" })
     .click();
-  await page.getByLabel("Name", { exact: true }).click();
   await page.getByLabel("Name", { exact: true }).fill("Bank1");
-  await page.getByLabel("Bank account holder name").click();
   await page.getByLabel("Bank account holder name").fill("Name Surname");
-  await page.getByLabel("Bank sort code").click();
   await page.getByLabel("Bank sort code").fill("123456");
-  await page.getByLabel("Bank account number").click();
   await page.getByLabel("Bank account number").fill("12345678");
   await page.getByRole("button", { name: "Confirm" }).click();
-  await page.getByRole("cell", { name: "Bank1" }).click();
+  const accountName = await page.getByRole("cell", { name: "Bank1" });
+  await expect(accountName).toBeVisible();
 });
