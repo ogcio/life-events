b5705ddabdbc57552cd95510f20908291aeb3458
GiuliaTeggi <giulia.teggi@nearform.com>
2024-03-25 11:13:27 +0000

    Update breadcrumbs styling

--- apps/web/app/[locale]/[event]/[...action]/shared/ActionBreadcrumb.tsx
+++ apps/web/app/[locale]/[event]/[...action]/shared/ActionBreadcrumb.tsx
@@ -8,13 +8,20 @@ type Props = {
 
 export default ({ action, step }: Props) => {
   const t = useTranslations("BreadcrumbsBySlug");
+  console.log("== ACTION ==", action);
+  console.log("== STEP ===", step);
   const actionElement =
     action.href !== undefined ? (
       <Link className="govie-breadcrumbs__link" href={action.href}>
         {t(action.slug)}
       </Link>
     ) : (
-      <span className="govie-breadcrumbs__link">{t(action.slug)}</span>
+      <span
+        className="govie-breadcrumbs__link"
+        style={{ pointerEvents: "none", textDecoration: "none" }}
+      >
+        {t(action.slug)}
+      </span>
     );
   return (
     <div className="govie-breadcrumbs" style={{ marginBottom: "30px" }}>
@@ -26,7 +33,12 @@ export default ({ action, step }: Props) => {
         </li>
         <li className="govie-breadcrumbs__list-item">{actionElement}</li>
         <li className="govie-breadcrumbs__list-item">
-          <span className="govie-breadcrumbs__link">{t(step)}</span>
+          <span
+            className="govie-breadcrumbs__link"
+            style={{ pointerEvents: "none", textDecoration: "none" }}
+          >
+            {t(step)}
+          </span>
         </li>
       </ol>
     </div>
