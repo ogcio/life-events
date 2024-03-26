d21372f7ce118785ddc655cc20303b38eaec7881
Ludwig Thurfjell <ludwig.thurfjell@gmail.com>
2024-03-26 09:54:10 +0100

    migration down

--- packages/messages/migrations/20240318121837-simplication-poc.js
+++ packages/messages/migrations/20240318121837-simplication-poc.js
@@ -38,11 +38,21 @@ exports.up = function (db) {
 };
 
 exports.down = function (db) {
-  db.runSql(`
-    DROP TABLE form_errors;
-    DROP TABLE events;
-    DROP TABLE email_message_states;
-  `);
+  var filePath = path.join(
+    __dirname,
+    "sqls",
+    "20240318121837-simplication-poc-down.sql",
+  );
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
+      if (err) return reject(err);
+      console.log("received data: " + data);
+
+      resolve(data);
+    });
+  }).then(function (data) {
+    return db.runSql(data);
+  });
 };
 
 exports._meta = {
