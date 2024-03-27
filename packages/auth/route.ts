75cbe04d03fd1d9b3adb8920cd7a833c0ef56709
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-03-27 17:14:35 +0100

    Revert "Fixed"
    This reverts commit f383a8475da2721da0f5ec0be1aa1cd5bb182749.

--- packages/auth/route.ts
+++ packages/auth/route.ts
@@ -1,7 +1,6 @@
 import { cookies } from "next/headers";
 import { decodeJwt, pgpool, PgSessions } from "./sessions";
 import { redirect, RedirectType } from "next/navigation";
-import { NextResponse } from "next/server";
 
 function getDomainForCookie(hostname: string) {
   const splitted = hostname.split(".");
@@ -27,19 +26,11 @@ function getSessionIdCookieConfig(req: Request, cookieValue: string) {
     path: "/",
   };
   const url = new URL(process.env.HOST_URL ?? req.url);
-  console.log(`Auth route. Current hostname: ${url.hostname}`);
   if (url.protocol === "https:") {
     return {
       ...cookieConfig,
       secure: true,
       sameSite: SAME_SITE_VALUES.NONE,
-    };
-  }
-
-  if (url.hostname !== "localhost") {
-    return {
-      ...cookieConfig,
-      domain: getDomainForCookie(url.hostname),
     };
   }
 
@@ -90,18 +81,13 @@ export default async function (req: Request) {
     userId: id,
   });
 
-  const or = new URL(process.env.HOST_URL ?? req.url).origin;
-  console.log(`Origin ${or}`);
-  const response = NextResponse.redirect(
-    is_public_servant ? or + "/admin" : or,
-    { status: 302 },
-  );
+  const cookiesConfig = getSessionIdCookieConfig(req, ssid);
+  console.log(`Auth route. Cookie config: ${JSON.stringify(cookiesConfig)}`);
+  cookies().set(cookiesConfig);
 
-  response.cookies.set("sessionId", ssid, getSessionIdCookieConfig(req, ssid));
+  if (is_public_servant) {
+    return redirect("/admin", RedirectType.replace);
+  }
 
-  console.log(
-    `Auth route. Cookie config: ${JSON.stringify(response.cookies.getAll())}`,
-  );
-
-  return response;
+  redirect("/", RedirectType.replace);
 }
