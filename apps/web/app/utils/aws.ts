0d9b17d8d9c6811953dd0f9df7c682dad925e70d
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-03-28 13:52:45 +0100

    Added method to check env keys

--- apps/web/app/utils/aws.ts
+++ apps/web/app/utils/aws.ts
@@ -1,11 +1,43 @@
-export const s3ClientConfig = {
-  region: "eu-west-1",
-  endpoint: process.env.S3_ENDPOINT,
-  forcePathStyle: true,
-  credentials: {
-    accessKeyId: "accessKeyId",
-    secretAccessKey: "accessKeyId",
-  },
+const checkKey = (
+  keyName: string,
+  errors: string[],
+  defaultValue?: string,
+): string => {
+  let value = process.env[keyName];
+  if (!value && !defaultValue) {
+    errors.push(keyName);
+    return "";
+  }
+
+  if (defaultValue) {
+    return defaultValue;
+  }
+
+  return value as string;
 };
 
-export const fileBucketName = "life-events-files"; // use env later?
+export const getS3ClientConfig = () => {
+  const errors = [] as string[];
+  const region = checkKey("S3_REGION", errors, "eu-west-1");
+  const endpoint = checkKey("S3_ENDPOINT", errors);
+  const accessKeyId = checkKey("S3_ACCESS_KEY_ID", errors);
+  const secretAccessKey = checkKey("S3_SECRET_ACCESS_KEY", errors);
+  const bucketName = checkKey("S3_BUCKET_NAME", errors, "life-events-files");
+
+  if (errors.length) {
+    throw new Error(`AWS Config. Missing following keys: ${errors.join(", ")}`);
+  }
+
+  return {
+    config: {
+      region,
+      endpoint,
+      forcePathStyle: true,
+      credentials: {
+        accessKeyId,
+        secretAccessKey,
+      },
+    },
+    bucketName,
+  };
+};
