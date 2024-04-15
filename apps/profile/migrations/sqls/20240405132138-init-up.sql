19af13b1a16f857abe194895690ba94ab0ef01b3
GiuliaTeggi <giulia.teggi@nearform.com>
2024-04-15 17:03:26 +0100

    Add addresses and entitlements migrations

--- apps/profile/migrations/sqls/20240405132138-init-up.sql
+++ apps/profile/migrations/sqls/20240405132138-init-up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS user_details(
     user_id UUID NOT NULL DEFAULT gen_random_uuid() UNIQUE,
-    title TEXT,
+    title TEXT NOT NULL,
     firstName TEXT NOT NULL,
     lastName TEXT NOT NULL,
     date_of_birth DATE NOT NULL,
@@ -8,7 +8,6 @@ CREATE TABLE IF NOT EXISTS user_details(
     gender TEXT NOT NULL,
     phone TEXT NOT NULL,
     email TEXT NOT NULL,
-    consent_to_prefill_data BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     primary key(user_id)
@@ -19,23 +18,22 @@ CREATE TABLE IF NOT EXISTS user_addresses (
     user_id UUID NOT NULL,
     address_line1 TEXT NOT NULL,
     address_line2 TEXT,
-    town TEXT NOT NULL,
-    county TEXT NOT NULL,
+    city TEXT NOT NULL,
     eirecode TEXT NOT NULL,
     is_primary BOOLEAN NOT NULL DEFAULT FALSE,
-    ownership_status TEXT,
-    start_date  TIMESTAMPTZ,
+    ownership_status TEXT NOT NULL,
+    start_date  TIMESTAMPTZ NOT NULL,
     end_date  TIMESTAMPTZ,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     FOREIGN KEY (user_id) REFERENCES user_details(user_id)
 );
 
-CREATE TABLE IF NOT EXISTS user_entitlements (
+CREATE TABLE IF NOT EXISTS entitlements (
     id UUID NOT NULL DEFAULT gen_random_uuid() UNIQUE,
     type TEXT NOT NULL, -- eventually could be an enum
-    issue_date TIMESTAMPTZ NOT NULL,
-    expiry_date TIMESTAMPTZ,
+    start_date TIMESTAMPTZ NOT NULL,
+    end_date TIMESTAMPTZ,
     document_number TEXT NOT NULL,
     user_id UUID NOT NULL,
     firstName TEXT NOT NULL, 
