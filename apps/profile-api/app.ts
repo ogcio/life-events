c254bbb49f8c40f5fa50ddcef24088de966b4d23
GiuliaTeggi <giulia.teggi@nearform.com>
2024-04-17 18:50:57 +0100

    Fix openapi tags

--- apps/profile-api/app.ts
+++ apps/profile-api/app.ts
@@ -37,7 +37,7 @@ export async function build(opts?: FastifyServerOptions) {
       },
       tags: [
         {
-          name: "Providers",
+          name: "Addresses",
         },
       ],
     },
