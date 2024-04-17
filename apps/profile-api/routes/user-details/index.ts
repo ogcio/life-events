7f48f22fd75251ee28a8fa7f95bcd13426b7e193
GiuliaTeggi <giulia.teggi@nearform.com>
2024-04-17 18:36:35 +0100

    Remove unneeded types

--- apps/profile-api/routes/user-details/index.ts
+++ apps/profile-api/routes/user-details/index.ts
@@ -1,5 +1,3 @@
 import { FastifyInstance } from "fastify";
-import { Type } from "@sinclair/typebox";
-import { HttpError } from "../../types/httpErrors";
 
 export default async function entitlements(app: FastifyInstance) {}
