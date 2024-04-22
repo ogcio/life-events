8cf3c2074a67b73aededdc19adae186056c1875f
Sara Zanellato <sara.zanellato@nearform.com>
2024-04-22 14:57:31 +0200

    update migrations

--- apps/payments/migrations/sqls/20240422095821-re-enable-authentication-transaction-up.sql
+++ apps/payments/migrations/sqls/20240422095821-re-enable-authentication-transaction-up.sql
@@ -3,6 +3,5 @@ BEGIN;
 ALTER TABLE payment_transactions DROP COLUMN user_data;
 
 ALTER TABLE payment_transactions ADD COLUMN user_id UUID; 
-ALTER TABLE payment_transactions ADD COLUMN user_ppsn TEXT;
 
 COMMIT;
