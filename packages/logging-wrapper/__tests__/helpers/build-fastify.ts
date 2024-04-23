c8c36934885fab92d32a8be4b93867ab913c5097
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-04-23 10:22:17 +0200

    feature(logging-wrapper): added tests helper

---
+++ packages/logging-wrapper/__tests__/helpers/build-fastify.ts
@@ -0,0 +1,42 @@
+import Fastify from "fastify";
+import {
+  getLoggingConfiguration,
+  initializeLoggingHooks,
+} from "../../src/fastify-logging-wrapper";
+import { DestinationStream } from "pino";
+import { createError } from "@fastify/error";
+
+export const buildFastify = (loggerDestination?: DestinationStream) => {
+  const server = Fastify({
+    ...getLoggingConfiguration(loggerDestination),
+  });
+
+  initializeLoggingHooks(server);
+
+  server.get("/ping", async (_request, _reply) => {
+    return { data: "pong\n" };
+  });
+
+  server.get("/error", async (request, _reply) => {
+    const parsed = request.query as { [x: string]: unknown };
+    const requestedStatusCode = Number(parsed["status_code"] ?? "500");
+    const requestedMessage = parsed["error_message"] ?? "WHOOOPS";
+
+    throw createError(
+      "CUSTOM_CODE",
+      requestedMessage as string,
+      requestedStatusCode as number,
+    )();
+  });
+
+  server.post("/logs", async (request, _reply) => {
+    const body = request.body as { [x: string]: unknown };
+    const logMessage = body.log_entry ?? "Default additional message";
+
+    request.log.info(logMessage);
+
+    return { data: { message: logMessage } };
+  });
+
+  return server;
+};
