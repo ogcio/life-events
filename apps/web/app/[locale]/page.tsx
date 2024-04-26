d877a3e664194e80ba1c4e033a5b2f4a5efd472d
Francesco Maida <francesco.maida@nearform.com>
2024-04-26 15:05:32 +0200

    test dockerfile change

--- apps/web/app/[locale]/page.tsx
+++ apps/web/app/[locale]/page.tsx
@@ -8,6 +8,7 @@ type Props = {
 };
 
 export default (props: Props) => {
+  console.log("PUBLIC_ENDPOINT", process.env.NEXT_PUBLIC_API_ENDPOINT);
   const path = `${props.params.locale}/${routes.events.slug}`;
   redirect(path, RedirectType.replace);
 };
