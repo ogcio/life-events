51107f86bf1dbe6b72ff93df5e3f2799e267104a
Ludwig Thufjell <ludwig.thurfjell@gmail.com>
2024-04-26 14:18:12 +0200

    gealic to gaelic (#173)

--- apps/profile/app/components/Header.tsx
+++ apps/profile/app/components/Header.tsx
@@ -141,7 +141,7 @@ export default () => {
                 }`.trim()}
                 href={new URL("/ga/" + path, process.env.HOST_URL).href}
               >
-                Gealic
+                Gaelic
               </Link>
             </div>
             <UserIcon />
