3b92726cd91a7b6be19d35052c729f9c6580e1e1
Francesco Maida <francesco.maida@nearform.com>
2024-04-29 09:12:24 +0200

    Static path dockerfile change

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,22 +1,18 @@
 import createNextIntlPlugin from "next-intl/plugin";
-import path from 'path'
-import * as url from 'url';
+import path from "path";
+import * as url from "url";
 
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
 const withNextIntl = createNextIntlPlugin("./i18n.ts");
-console.log(path.join(__dirname, '../../'),);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["auth", "design-system", "feature-flags", "messages"],
-  output: 'standalone',
+  output: "standalone",
   experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../../'),
-  }
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+  },
 };
 
 export default withNextIntl(nextConfig);
-
-
-
