287a4dac02f6977ebe600ba0a62d721048ddd759
Francesco Maida <francesco.maida@nearform.com>
2024-05-13 17:15:54 +0200

    Add auth service deploy

---
+++ apps/web/app/[locale]/api/logout/route.ts
@@ -0,0 +1,25 @@
+import { cookies } from "next/headers";
+import { redirect, RedirectType } from "next/navigation";
+import { PgSessions } from "auth/sessions";
+
+export async function GET() {
+  const sessionCookie = cookies().get("sessionId");
+  if (sessionCookie) {
+    PgSessions.delete(sessionCookie.value);
+    cookies().delete(sessionCookie.name);
+  }
+
+  const loginUrl = process.env.LOGIN_URL;
+
+  if (!loginUrl) {
+    throw Error("Missing env var LOGIN_URL");
+  }
+
+  const url = new URL(loginUrl);
+  url.searchParams.append(
+    "return_url",
+    new URL("/en/api/auth", process.env.HOST_URL).href,
+  );
+
+  redirect(url.href, RedirectType.replace);
+}
