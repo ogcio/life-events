7e08051558c6b863b15726f46f2043b950b29d86
Norbert Nagy <norbert.nagy@nearform.com>
2024-05-16 16:51:59 +0300

    Updated migration scripts

--- apps/payments/migrations/sqls/20240507140208-update-provider-name-field-unique-up.sql
+++ apps/payments/migrations/sqls/20240507140208-update-provider-name-field-unique-up.sql
@@ -1,6 +1,9 @@
-BEGIN;
-
-ALTER TABLE payment_providers
-ADD CONSTRAINT unique_provider_name UNIQUE (user_id, provider_name);
-
-COMMIT;
+DO $$ BEGIN
+    IF NOT EXISTS (
+        SELECT FROM pg_constraint 
+        WHERE conname = 'unique_provider_name')
+    THEN 
+        ALTER TABLE payment_providers
+        ADD CONSTRAINT unique_provider_name UNIQUE (user_id, provider_name);
+    END IF;
+END $$;
