07e87508eb1d19746b58f233b9c6ce2a42c5304a
Norbert Nagy <norbert.nagy@nearform.com>
2024-05-16 17:45:16 +0300

    Fix

--- apps/payments/migrations/sqls/20240507140208-update-provider-name-field-unique-up.sql
+++ apps/payments/migrations/sqls/20240507140208-update-provider-name-field-unique-up.sql
@@ -1,4 +1,12 @@
 DO $$ BEGIN
+    UPDATE payment_providers as providers
+    SET provider_name = providers.provider_name || '_' || floor(random()*1000) + 1
+    FROM (
+        SELECT provider_id, provider_name, row_number(*) over (partition by provider_name order by provider_id) as rn
+        FROM public.payment_providers
+    ) dublicate
+    where providers.provider_id = dublicate.provider_id AND dublicate.rn > 1;
+
     IF NOT EXISTS (
         SELECT FROM pg_constraint 
         WHERE conname = 'unique_provider_name')
