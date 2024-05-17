caf10232663f027f044258baf1064da0954b6647
Sara Zanellato <sara.zanellato@nearform.com>
2024-05-17 11:57:14 +0200

    Reuse language switch component

--- apps/payments/app/components/Header/LanguageSwitch.tsx
+++ apps/payments/app/components/Header/LanguageSwitch.tsx
@@ -1,16 +1,25 @@
 import ds from "design-system";
 import { headers } from "next/headers";
+import styles from "./Header.module.scss";
 
-export default () => {
+type Theme = "dark" | "light";
+
+const colors: Record<Theme, string> = {
+  dark: ds.colours.ogcio.white,
+  light: ds.colours.ogcio.darkGreen,
+};
+
+export default ({ theme }: { theme: Theme }) => {
   const pathSlice = headers().get("x-pathname")?.split("/") ?? [];
   const path = pathSlice.slice(2)?.join("/") || "";
 
   return (
-    <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+    <div className={styles.languagesContainer}>
       <a
-        className={`govie-link govie-link--inverse govie-link--no-underline ${
+        className={`govie-link govie-link--no-underline ${
           pathSlice.at(1) === "en" ? "govie-!-font-weight-bold" : ""
         }`.trim()}
+        style={{ color: colors[theme] }}
         href={new URL("/en/" + path, process.env.HOST_URL).href}
       >
         English
@@ -19,14 +28,15 @@ export default () => {
         style={{
           height: "14px",
           width: "1px",
-          borderLeft: `1px solid ${ds.colours.ogcio.white}`,
+          borderLeft: `1px solid ${ds.colours.ogcio.darkGreen}`,
         }}
       />
 
       <a
-        className={`govie-link govie-link--inverse govie-link--no-underline  ${
+        className={`govie-link govie-link--no-underline  ${
           pathSlice.at(1) === "ga" ? "govie-!-font-weight-bold" : ""
         }`.trim()}
+        style={{ color: colors[theme] }}
         href={new URL("/ga/" + path, process.env.HOST_URL).href}
       >
         Gaelic
