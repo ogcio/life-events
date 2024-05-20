0b4a5ad5358c5cf5767653e6de622b464d9bdfb9
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-05-20 10:48:07 +0200

    Added migrations

---
+++ packages/messages/migrations/sqls/20240520080749-add-users-data-up.sql
@@ -0,0 +1,27 @@
+CREATE TABLE
+    users (
+        id uuid NOT NULL DEFAULT gen_random_uuid () PRIMARY KEY,
+        user_profile_id uuid,
+        importer_organisation_id uuid NOT NULL,
+        user_status text NOT NULL DEFAULT 'pending'
+    );
+
+CREATE TABLE
+    organisations_users (
+        organisation_id uuid NOT NULL,
+        user_id uuid NOT NULL REFERENCES users (id),
+        invitation_status text NOT NULL DEFAULT 'pending',
+        invitation_sent_at timestamptz,
+        invitation_feedback_at timestamptz,
+        preferred_transports text[],
+        UNIQUE (organisation_id, user_id)
+
+    );
+
+CREATE TABLE 
+    users_imports (
+        organisation_id uuid NOT NULL,
+        imported_at timestamptz DEFAULT now(),
+        users_data jsonb NOT NULL,
+        import_channel text NOT NULL DEFAULT 'api'
+    );
