e19c484cd50d9a89f6b255b9a4ce3685345e38b7
alfonsograziano <alfonsohack0019@gmail.com>
2024-05-21 10:55:32 +0200

    Change styles to use globals

--- apps/home/app/[locale]/page.tsx
+++ apps/home/app/[locale]/page.tsx
@@ -1,6 +1,6 @@
 import "design-system/dist/style.css";
 import "design-system/dist/esm/index.css";
-import "./page.css";
+import "./page.scss";
 import Header from "./Header";
 import Footer from "./Footer";
 import { getTranslations } from "next-intl/server";
