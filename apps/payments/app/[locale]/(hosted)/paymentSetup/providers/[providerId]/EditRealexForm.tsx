cf8061d8fe9e55e84dc3a783fe3114993b99b10a
Norbert Nagy <nagy.norbert1989@gmail.com>
2024-05-23 18:16:31 +0300

    Payment provider form validations (#299)
    * Payment provider form validations
    
    * Update
    
    * Update

--- apps/payments/app/[locale]/(hosted)/paymentSetup/providers/[providerId]/EditRealexForm.tsx
+++ apps/payments/app/[locale]/(hosted)/paymentSetup/providers/[providerId]/EditRealexForm.tsx
@@ -1,54 +1,115 @@
 import { redirect } from "next/navigation";
 import EditProviderForm from "./EditProviderForm";
-import type { RealexProvider, RealexData } from "../types";
+import type { RealexProvider } from "../types";
 import { getTranslations } from "next-intl/server";
 import RealexFields from "../add-realex/RealexFields";
-import { PgSessions } from "auth/sessions";
 import { Payments } from "building-blocks-sdk";
-import { errorHandler } from "../../../../../utils";
+import getRequestConfig from "../../../../../../i18n";
+import { errorHandler, getValidationErrors } from "../../../../../utils";
+import { AbstractIntlMessages, NextIntlClientProvider } from "next-intl";
+import { realexValidationMap } from "../../../../../validationMaps";
+import { RealexFormState } from "../add-realex/page";
 
 type Props = {
   provider: RealexProvider;
+  userId: string;
+  locale: string;
 };
 
-export default async ({ provider }: Props) => {
+export default async ({ provider, userId, locale }: Props) => {
   const t = await getTranslations("PaymentSetup.AddRealex");
+  const { messages } = await getRequestConfig({ locale });
 
-  async function updateProvider(formData: FormData) {
+  const errorFieldMapping = realexValidationMap(t);
+
+  async function handleSubmit(
+    prevState: FormData,
+    formData: FormData,
+  ): Promise<RealexFormState> {
     "use server";
+    const nameField = formData.get("provider_name") as string;
+    const merchantIdField = formData.get("merchant_id") as string;
+    const sharedSecretField = formData.get("shared_secret") as string;
 
-    const { userId } = await PgSessions.get();
-
-    const providerName = formData.get("provider_name") as unknown as string;
-    const merchantId = formData.get("merchant_id") as unknown as string;
-    const sharedSecret = formData.get("shared_secret") as unknown as string;
-    const providerData = {
-      merchantId,
-      sharedSecret,
+    const formResult = {
+      errors: {},
+      defaultState: {
+        providerName: nameField,
+        merchantId: merchantIdField,
+        sharedSecret: sharedSecretField,
+      },
     };
 
-    const { error } = await new Payments(userId).updateProvider(provider.id, {
-      name: providerName,
-      data: providerData,
-      type: provider.type,
-      status: provider.status,
-    });
+    const action = formData.get("action");
+    let providerData;
+    switch (action) {
+      case "enable":
+        providerData = {
+          name: provider.name,
+          data: provider.data,
+          type: provider.type,
+          status: "connected",
+        };
+        break;
+      case "disable":
+        providerData = {
+          name: provider.name,
+          data: provider.data,
+          type: provider.type,
+          status: "disconnected",
+        };
+        break;
+      default:
+        providerData = {
+          name: nameField,
+          data: {
+            merchantId: merchantIdField,
+            sharedSecret: sharedSecretField,
+          },
+          type: provider.type,
+          status: provider.status,
+        };
+    }
+
+    const { data: result, error } = await new Payments(userId).updateProvider(
+      provider.id,
+      providerData,
+    );
 
     if (error) {
       errorHandler(error);
     }
 
-    redirect("./");
+    if (result) {
+      redirect("./");
+    }
+
+    if (error?.validation) {
+      formResult.errors = getValidationErrors(
+        error.validation,
+        errorFieldMapping,
+      );
+    }
+
+    return formResult;
   }
 
   return (
-    <EditProviderForm provider={provider} updateProviderAction={updateProvider}>
-      <h1 className="govie-heading-l">{t("editTitle")}</h1>
-      <RealexFields
-        providerName={provider.name}
-        merchantId={(provider.data as RealexData).merchantId}
-        sharedSecret={(provider.data as RealexData).sharedSecret}
-      />
-    </EditProviderForm>
+    <NextIntlClientProvider
+      messages={messages?.["PaymentSetup"] as AbstractIntlMessages}
+    >
+      <EditProviderForm
+        provider={provider}
+        action={handleSubmit}
+        formComponent={RealexFields}
+        defaultState={{
+          providerName: provider.name,
+          merchantId: provider.data.merchantId,
+          sharedSecret: provider.data.sharedSecret,
+        }}
+      >
+        <h1 className="govie-heading-l">{t("editTitle")}</h1>
+      </EditProviderForm>
+    </NextIntlClientProvider>
   );
 };
