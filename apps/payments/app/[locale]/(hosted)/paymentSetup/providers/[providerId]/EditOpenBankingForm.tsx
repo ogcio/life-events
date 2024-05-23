bbd54602fea1d99f23dfc281176cc4c8e775155c
Norbert Nagy <nagy.norbert1989@gmail.com>
2024-05-23 18:16:31 +0300

    Payment provider form validations (#299)
    * Payment provider form validations
    
    * Update
    
    * Update

--- apps/payments/app/[locale]/(hosted)/paymentSetup/providers/[providerId]/EditOpenBankingForm.tsx
+++ apps/payments/app/[locale]/(hosted)/paymentSetup/providers/[providerId]/EditOpenBankingForm.tsx
@@ -3,47 +3,115 @@ import OpenBankingFields from "../add-openbanking/OpenBankingFields";
 import EditProviderForm from "./EditProviderForm";
 import type { OpenBankingProvider } from "../types";
 import { getTranslations } from "next-intl/server";
-import { PgSessions } from "auth/sessions";
 import { Payments } from "building-blocks-sdk";
-import { errorHandler } from "../../../../../utils";
+import getRequestConfig from "../../../../../../i18n";
+import { errorHandler, getValidationErrors } from "../../../../../utils";
+import { AbstractIntlMessages, NextIntlClientProvider } from "next-intl";
+import { openBankingValidationMap } from "../../../../../validationMaps";
+import { OpenBankingFormState } from "../add-openbanking/page";
 
 type Props = {
   provider: OpenBankingProvider;
+  userId: string;
+  locale: string;
 };
 
-export default async ({ provider }: Props) => {
+export default async ({ provider, userId, locale }: Props) => {
   const t = await getTranslations("PaymentSetup.AddOpenbanking");
+  const { messages } = await getRequestConfig({ locale });
 
-  async function updateProvider(formData: FormData) {
+  const errorFieldMapping = openBankingValidationMap(t);
+
+  async function handleSubmit(
+    prevState: FormData,
+    formData: FormData,
+  ): Promise<OpenBankingFormState> {
     "use server";
+    const nameField = formData.get("provider_name") as string;
+    const accountHolderNameField = formData.get(
+      "account_holder_name",
+    ) as string;
+    const ibanField = (formData.get("iban") as string).replaceAll(" ", "");
 
-    const { userId } = await PgSessions.get();
-
-    const { error } = await new Payments(userId).updateProvider(provider.id, {
-      name: formData.get("provider_name") as string,
-      data: {
-        iban: (formData.get("iban") as string).replaceAll(" ", ""),
-        accountHolderName: formData.get("account_holder_name") as string,
+    const formResult = {
+      errors: {},
+      defaultState: {
+        providerName: nameField,
+        accountHolderName: accountHolderNameField,
+        iban: ibanField,
       },
-      type: provider.type,
-      status: provider.status,
-    });
+    };
+
+    const action = formData.get("action");
+    let providerData;
+    switch (action) {
+      case "enable":
+        providerData = {
+          name: provider.name,
+          data: provider.data,
+          type: provider.type,
+          status: "connected",
+        };
+        break;
+      case "disable":
+        providerData = {
+          name: provider.name,
+          data: provider.data,
+          type: provider.type,
+          status: "disconnected",
+        };
+        break;
+      default:
+        providerData = {
+          name: nameField,
+          data: {
+            iban: ibanField,
+            accountHolderName: accountHolderNameField,
+          },
+          type: provider.type,
+          status: provider.status,
+        };
+    }
+
+    const { data: result, error } = await new Payments(userId).updateProvider(
+      provider.id,
+      providerData,
+    );
 
     if (error) {
       errorHandler(error);
     }
 
-    redirect("./");
+    if (result) {
+      redirect("./");
+    }
+
+    if (error?.validation) {
+      formResult.errors = getValidationErrors(
+        error.validation,
+        errorFieldMapping,
+      );
+    }
+
+    return formResult;
   }
 
   return (
-    <EditProviderForm provider={provider} updateProviderAction={updateProvider}>
-      <h1 className="govie-heading-l">{t("editTitle")}</h1>
-      <OpenBankingFields
-        providerName={provider.name}
-        accountHolderName={provider.data.accountHolderName}
-        iban={provider.data.iban}
-      />
-    </EditProviderForm>
+    <NextIntlClientProvider
+      messages={messages?.["PaymentSetup"] as AbstractIntlMessages}
+    >
+      <EditProviderForm
+        provider={provider}
+        action={handleSubmit}
+        formComponent={OpenBankingFields}
+        defaultState={{
+          providerName: provider.name,
+          accountHolderName: provider.data.accountHolderName,
+          iban: provider.data.iban,
+        }}
+      >
+        <h1 className="govie-heading-l">{t("editTitle")}</h1>
+      </EditProviderForm>
+    </NextIntlClientProvider>
   );
 };
---
+++ apps/payments/app/validationMaps.ts
@@ -0,0 +1,124 @@
+import { ValidationErrorTypes, ValidationFieldMap } from "./utils";
+
+export const bankTransferValidationMap = (t): ValidationFieldMap => {
+  return {
+    name: {
+      field: "providerName",
+      errorMessage: {
+        [ValidationErrorTypes.REQUIRED]: t("nameRequired"),
+      },
+    },
+    iban: {
+      field: "iban",
+      errorMessage: {
+        [ValidationErrorTypes.REQUIRED]: t("ibanRequired"),
+        [ValidationErrorTypes.INVALID]: t("ibanInvalid"),
+      },
+    },
+    accountHolderName: {
+      field: "accountHolderName",
+      errorMessage: {
+        [ValidationErrorTypes.REQUIRED]: t("accountHolderNameRequired"),
+      },
+    },
+  };
+};
+
+export const openBankingValidationMap = (t): ValidationFieldMap => {
+  return {
+    name: {
+      field: "providerName",
+      errorMessage: {
+        [ValidationErrorTypes.REQUIRED]: t("nameRequired"),
+      },
+    },
+    iban: {
+      field: "iban",
+      errorMessage: {
+        [ValidationErrorTypes.REQUIRED]: t("ibanRequired"),
+        [ValidationErrorTypes.INVALID]: t("ibanInvalid"),
+      },
+    },
+    accountHolderName: {
+      field: "accountHolderName",
+      errorMessage: {
+        [ValidationErrorTypes.REQUIRED]: t("accountHolderNameRequired"),
+      },
+    },
+  };
+};
+
+export const realexValidationMap = (t): ValidationFieldMap => {
+  return {
+    name: {
+      field: "providerName",
+      errorMessage: {
+        [ValidationErrorTypes.REQUIRED]: t("nameRequired"),
+      },
+    },
+    merchantId: {
+      field: "merchantId",
+      errorMessage: {
+        [ValidationErrorTypes.REQUIRED]: t("merchantIdRequired"),
+      },
+    },
+    sharedSecret: {
+      field: "sharedSecret",
+      errorMessage: {
+        [ValidationErrorTypes.REQUIRED]: t("sharedSecretRequired"),
+      },
+    },
+  };
+};
+
+export const stripeValidationMap = (t): ValidationFieldMap => {
+  return {
+    name: {
+      field: "providerName",
+      errorMessage: {
+        [ValidationErrorTypes.REQUIRED]: t("nameRequired"),
+      },
+    },
+    liveSecretKey: {
+      field: "liveSecretKey",
+      errorMessage: {
+        [ValidationErrorTypes.REQUIRED]: t("liveSecretKeyRequired"),
+      },
+    },
+    livePublishableKey: {
+      field: "livePublishableKey",
+      errorMessage: {
+        [ValidationErrorTypes.REQUIRED]: t("livePublishableKeyRequired"),
+      },
+    },
+  };
+};
+
+export const paymentRequestValidationMap = (t): ValidationFieldMap => {
+  return {
+    title: {
+      field: "title",
+      errorMessage: {
+        [ValidationErrorTypes.REQUIRED]: t("titleRequired"),
+      },
+    },
+    reference: {
+      field: "reference",
+      errorMessage: {
+        [ValidationErrorTypes.REQUIRED]: t("referenceRequired"),
+      },
+    },
+    amount: {
+      field: "amount",
+      errorMessage: {
+        [ValidationErrorTypes.REQUIRED]: t("amountRequired"),
+      },
+    },
+    redirectUrl: {
+      field: "redirectUrl",
+      errorMessage: {
+        [ValidationErrorTypes.REQUIRED]: t("redirectUrlRequired"),
+      },
+    },
+  };
+};
