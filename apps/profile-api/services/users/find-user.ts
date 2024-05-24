38432277c912ff81c67a42000060a2ab8306e39e
SamSalvatico <40636569+SamSalvatico@users.noreply.github.com>
2024-05-24 12:38:13 +0200

    chore(user-profile): improved queries (#314)
    * chore(user-profile): improved queries
    
    * Removed useless const

--- apps/profile-api/services/users/find-user.ts
+++ apps/profile-api/services/users/find-user.ts
@@ -1,57 +1,10 @@
 import { Pool, PoolClient } from "pg";
-import {
-  FindUserParams,
-  FoundUser,
-  MatchQuality,
-  UserDetailColumnsMapping,
-} from "../../types/schemaDefinitions";
+import { FindUserParams, FoundUser } from "../../types/schemaDefinitions";
 import { isNativeError } from "util/types";
 import { createError } from "@fastify/error";
 
 type WhereClauseTypes = string | number | null | boolean;
 type PartialFoundUser = Omit<FoundUser, "matchQuality">;
-const buildFindQuery = (whereClauses: {
-  [x: string]:
-    | WhereClauseTypes
-    | {
-        value: WhereClauseTypes;
-        operator: "=" | "LIKE" | "ILIKE";
-        isDate: boolean;
-      };
-}): { query: string; values: WhereClauseTypes[] } => {
-  let baseQuery = `
-        SELECT 
-            user_id as "id",
-            firstname as "firstname",
-            lastname as "lastname"
-        FROM 
-            user_details
-    `;
-
-  const queryClauses = [];
-  let whereValuesIndex = 1;
-  const whereValues = [];
-  for (const fieldName in whereClauses) {
-    let operator = "=";
-    let value = whereClauses[fieldName];
-    let date = "";
-    if (typeof value === "object") {
-      operator = value!.operator;
-      date = value!.isDate ? "::date" : "";
-      value = value!.value;
-    }
-    queryClauses.push(
-      ` ${fieldName} ${operator} $${whereValuesIndex++}${date} `,
-    );
-    whereValues.push(value);
-  }
-  if (whereValues.length > 0) {
-    baseQuery = `${baseQuery} WHERE ${queryClauses.join("AND")}`;
-  }
-  baseQuery = `${baseQuery} LIMIT 1`;
-
-  return { query: baseQuery, values: whereValues };
-};
 
 export const findUser = async (params: {
   pool: Pool;
@@ -66,6 +19,7 @@ export const findUser = async (params: {
     findByPhoneNumber,
     findByBirthDate,
   ];
+
   try {
     for (const method of orderedMethodsToInvoke) {
       const methodResult = await method({ client, findUserParams });
@@ -80,130 +34,128 @@ export const findUser = async (params: {
   return undefined;
 };
 
-const clearField = (valueToClear: string | undefined): string | undefined =>
-  valueToClear && valueToClear.trim().length > 0
-    ? valueToClear.trim()
-    : undefined;
-
-const clearFields = (params: FindUserParams): FindUserParams => ({
-  ppsn: clearField(params.ppsn),
-  phone: clearField(params.phone),
-  lastname: clearField(params.lastname),
-  firstname: clearField(params.firstname),
-  dateOfBirth: clearField(params.dateOfBirth),
-  email: clearField(params.email),
-  gender: clearField(params.gender),
-});
-
-const findBy = async (params: {
-  matchQuality: MatchQuality;
-  fields: {
-    name: string;
-    operator?: "LIKE" | "ILIKE" | "=";
-    mandatory: boolean;
-    column?: string;
-    is_date?: boolean;
-  }[];
-  client: PoolClient;
-  findUserParams: FindUserParams;
-}) => {
-  const { matchQuality, fields, client, findUserParams } = params;
-  const toQuery: {
-    [x: string]: {
-      value: WhereClauseTypes;
-      operator: "=" | "LIKE" | "ILIKE";
-      isDate: boolean;
-    };
-  } = {};
-  const indexedUserParams: { [x: string]: undefined | string } = findUserParams;
-  const indexedColumnsMapping: { [x: string]: string } =
-    UserDetailColumnsMapping;
-  for (const field of fields) {
-    // if the field is needed for querying but it is empty
-    // we don't run the query and return not found user
-    if (!indexedUserParams[field.name] && field.mandatory) {
-      return undefined;
-    }
-
-    if (!field.column && !(field.name in indexedColumnsMapping)) {
-      return undefined;
-    }
-
-    if (indexedUserParams[field.name]) {
-      toQuery[field.column ?? indexedColumnsMapping[field.name]] = {
-        operator: field.operator ?? "=",
-        value: indexedUserParams[field.name]!,
-        isDate: field.is_date ?? false,
-      };
-    }
-  }
-
-  const found = await runFindQuery({ client, ...buildFindQuery(toQuery) });
-
-  if (!found) {
-    return found;
-  }
-
-  return { ...found, matchQuality };
-};
-
 const findByPhoneNumber = async (params: {
   client: PoolClient;
   findUserParams: FindUserParams;
-}): Promise<FoundUser | undefined> =>
-  findBy({
-    ...params,
-    matchQuality: "exact",
-    fields: [
-      { name: "firstname", operator: "ILIKE", mandatory: true },
-      { name: "lastname", operator: "ILIKE", mandatory: true },
-      { name: "phone", operator: "ILIKE", mandatory: true },
-    ],
+}): Promise<FoundUser | undefined> => {
+  const userParams = params.findUserParams;
+  if (!userParams.firstname || !userParams.lastname || !userParams.phone) {
+    return undefined;
+  }
+
+  const query = buildFindQuery([
+    "firstname ILIKE $1",
+    "lastname ILIKE $2",
+    "phone ILIKE $3",
+  ]);
+
+  const found = await runFindQuery({
+    client: params.client,
+    query,
+    values: [userParams.firstname, userParams.lastname, userParams.phone],
   });
+
+  return found ? { ...found, matchQuality: "exact" } : undefined;
+};
 
 const findByBirthDate = async (params: {
   client: PoolClient;
   findUserParams: FindUserParams;
-}): Promise<FoundUser | undefined> =>
-  findBy({
-    ...params,
-    matchQuality: "approximate",
-    fields: [
-      { name: "firstname", operator: "ILIKE", mandatory: true },
-      { name: "lastname", operator: "ILIKE", mandatory: true },
-      {
-        name: "dateOfBirth",
-        operator: "=",
-        mandatory: true,
-        column: "date_of_birth::date",
-        is_date: true,
-      },
-    ],
+}): Promise<FoundUser | undefined> => {
+  const userParams = params.findUserParams;
+  if (
+    !userParams.firstname ||
+    !userParams.lastname ||
+    !userParams.dateOfBirth
+  ) {
+    return undefined;
+  }
+
+  // We need to set the same timezone in both Node server
+  // and query to make the comparison between dates the same
+  // because can happen that, using different TZ, PGsql compares
+  // the "date_of_birth" column using the day before
+  // e.g. 1990-01-10 00:00:00.000 +0100 becomes 1990-01-09
+  const oldTz = process.env.TZ;
+  const timezoneToUse = "Europe/Rome";
+  process.env.TZ = timezoneToUse;
+
+  const query = buildFindQuery([
+    "firstname ILIKE $1",
+    "lastname ILIKE $2",
+    `to_char(timezone('${timezoneToUse}',"date_of_birth"),'YYYY-MM-DD') = $3`,
+  ]);
+
+  const found = await runFindQuery({
+    client: params.client,
+    query,
+    values: [userParams.firstname, userParams.lastname, userParams.dateOfBirth],
   });
+
+  process.env.TZ = oldTz;
+
+  return found ? { ...found, matchQuality: "approximate" } : undefined;
+};
 
 const findByEmailAddress = async (params: {
   client: PoolClient;
   findUserParams: FindUserParams;
-}): Promise<FoundUser | undefined> =>
-  findBy({
-    ...params,
-    matchQuality: "exact",
-    fields: [
-      { name: "firstname", operator: "ILIKE", mandatory: true },
-      { name: "lastname", operator: "ILIKE", mandatory: true },
-      { name: "email", operator: "ILIKE", mandatory: true },
-    ],
+}): Promise<FoundUser | undefined> => {
+  const userParams = params.findUserParams;
+  if (!userParams.firstname || !userParams.lastname || !userParams.email) {
+    return undefined;
+  }
+
+  const query = buildFindQuery([
+    "firstname ILIKE $1",
+    "lastname ILIKE $2",
+    "email ILIKE $3",
+  ]);
+
+  const found = await runFindQuery({
+    client: params.client,
+    query,
+    values: [userParams.firstname, userParams.lastname, userParams.email],
   });
+
+  return found ? { ...found, matchQuality: "exact" } : undefined;
+};
 
 const findByPpsn = async (params: {
   client: PoolClient;
   findUserParams: FindUserParams;
-}): Promise<FoundUser | undefined> =>
-  findBy({
-    ...params,
-    matchQuality: "exact",
-    fields: [{ name: "ppsn", mandatory: true, operator: "ILIKE" }],
+}): Promise<FoundUser | undefined> => {
+  if (!params.findUserParams.ppsn) {
+    return undefined;
+  }
+
+  const query = buildFindQuery(["ppsn ILIKE $1"]);
+
+  const found = await runFindQuery({
+    client: params.client,
+    query,
+    values: [params.findUserParams.ppsn],
   });
+
+  return found ? { ...found, matchQuality: "exact" } : undefined;
+};
+
+const buildFindQuery = (whereClauses: string[]): string => {
+  let query = `
+        SELECT 
+            user_id as "id",
+            firstname as "firstname",
+            lastname as "lastname"
+        FROM 
+            user_details
+    `;
+
+  if (whereClauses.length > 0) {
+    query = `${query} WHERE ${whereClauses.join(" AND ")}`;
+  }
+
+  return `${query} LIMIT 1`;
+};
 
 const runFindQuery = async (params: {
   client: PoolClient;
@@ -228,3 +180,18 @@ const runFindQuery = async (params: {
     throw toOutput;
   }
 };
+
+const clearField = (valueToClear: string | undefined): string | undefined =>
+  valueToClear && valueToClear.trim().length > 0
+    ? valueToClear.trim()
+    : undefined;
+
+const clearFields = (params: FindUserParams): FindUserParams => ({
+  ppsn: clearField(params.ppsn),
+  phone: clearField(params.phone),
+  lastname: clearField(params.lastname),
+  firstname: clearField(params.firstname),
+  dateOfBirth: clearField(params.dateOfBirth),
+  email: clearField(params.email),
+  gender: clearField(params.gender),
+});
