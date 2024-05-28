62bea7d8a8131d16cb74a2090da027bdd39ef7d6
Norbert Nagy <norbert.nagy@nearform.com>
2024-05-28 11:00:33 +0300

    Refactoring payments landing page

--- apps/payments/app/[locale]/info/page.tsx
+++ apps/payments/app/[locale]/info/page.tsx
@@ -1,5 +1,6 @@
 import "design-system/dist/style.css";
 import "design-system/dist/esm/index.css";
+import "./page.scss";
 import Header from "./Header";
 import Footer from "./Footer";
 import { getTranslations } from "next-intl/server";
@@ -9,6 +10,11 @@ import integration from "../../../public/landingPage/integration.png";
 import interfaceCitizen from "../../../public/landingPage/interfaceCitizen.png";
 import interfacePublicServant from "../../../public/landingPage/interfacePublicServant.png";
 import security from "../../../public/landingPage/security.png";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Building Blocks",
+};
 
 type Props = {
   params: {
@@ -21,18 +27,10 @@ export default async (props: Props) => {
 
   return (
     <html lang={props.params.locale}>
-      <body
-        style={{
-          margin: "unset",
-          minHeight: "100vh",
-          position: "relative",
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
+      <body className="page-wrapper">
         <Header />
 
-        <div className="width-container">
+        <div className="govie-width-container custom-width-container">
           <div className="govie-phase-banner">
             <p className="govie-phase-banner__content">
               <strong className="govie-tag govie-phase-banner__content__tag">
@@ -52,162 +50,16 @@ export default async (props: Props) => {
               </span>
             </p>
           </div>
-          <div style={{ width: "100%", margin: "0 auto", paddingTop: "40px" }}>
-            <div
-              style={{
-                display: "flex",
-                gap: "50px",
-                flexWrap: "wrap",
-              }}
-            >
-              <div style={{ flex: "1 1 auto", maxWidth: "700px" }}>
-                <h1 className="govie-heading-l">
-                  {t("sections.main.serviceName")}
-                </h1>
 
-                <h2 className="govie-heading-l">{t("sections.main.title")}</h2>
-                <p className="govie-body">{t("sections.main.description")}</p>
-                <a
-                  href={`${process.env.NEXT_PUBLIC_FORMS_URL}${props.params.locale}/664b6de45f7c9800231daf22`}
-                >
-                  <button
-                    id="button"
-                    data-module="govie-button"
-                    className="govie-button govie-button--primary"
-                  >
-                    {t("sections.main.cta")}
-                  </button>
-                </a>
-              </div>
-              <div style={{ flex: "1 1 auto", maxWidth: "700px" }}>
-                <Image
-                  src={hero}
-                  alt={t("sections.main.title")}
-                  layout="responsive"
-                />
-              </div>
-            </div>
-            <div className="govie-grid-column-full">
-              <hr className="govie-section-break govie-section-break--visible govie-section-break--xl" />
-            </div>
-            <h2 className="govie-heading-l">{t("benefits")}</h2>
+          <hr className="govie-section-break  govie-section-break--m" />
 
-            <div
-              style={{
-                display: "flex",
-                gap: "50px",
-                flexWrap: "wrap",
-              }}
-            >
-              <div style={{ flex: "1 1 auto", maxWidth: "700px" }}>
-                <h2 className="govie-heading-m">
-                  {t("sections.integration.title")}
-                </h2>
-                <p className="govie-body">
-                  {t("sections.integration.description")}
-                </p>
-              </div>
-              <div style={{ flex: "1 1 auto", maxWidth: "500px" }}>
-                <Image
-                  src={integration}
-                  alt={t("sections.integration.title")}
-                  layout="responsive"
-                />
-              </div>
-            </div>
-
-            <hr className="govie-section-break govie-section-break--visible govie-section-break--xl" />
-
-            <div
-              style={{
-                display: "flex",
-                gap: "50px",
-                flexWrap: "wrap",
-              }}
-            >
-              <div style={{ flex: "1 1 auto", maxWidth: "700px" }}>
-                <h2 className="govie-heading-m">
-                  {t("sections.security.title")}
-                </h2>
-                <p className="govie-body">
-                  {t("sections.security.description")}
-                </p>
-              </div>
-              <div style={{ flex: "1 1 auto", maxWidth: "500px" }}>
-                <Image
-                  src={security}
-                  alt={t("sections.security.title")}
-                  layout="responsive"
-                />
-              </div>
-            </div>
-
-            <hr className="govie-section-break govie-section-break--visible govie-section-break--xl" />
-
-            <div>
-              <div>
-                <h2 className="govie-heading-m">
-                  {t("sections.interface.title")}
-                </h2>
-                <p className="govie-body">
-                  {t("sections.interface.description")}
-                </p>
-              </div>
-              <div
-                style={{
-                  display: "flex",
-                  gap: "50px",
-                  flexWrap: "wrap",
-                  justifyContent: "space-around",
-                }}
-              >
-                <div style={{ flex: "1 1 auto", maxWidth: "400px" }}>
-                  <Image
-                    src={interfaceCitizen}
-                    alt={t("sections.interface.citizenPOV")}
-                    layout="responsive"
-                  />
-                  <p className="govie-body">
-                    {t("sections.interface.citizenPOV")}
-                  </p>
-                </div>
-                <div style={{ flex: "1 1 auto", maxWidth: "400px" }}>
-                  <Image
-                    src={interfacePublicServant}
-                    alt={t("sections.interface.publicServantPOV")}
-                    layout="responsive"
-                  />
-                  <p className="govie-body">
-                    {t("sections.interface.publicServantPOV")}
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <hr className="govie-section-break govie-section-break--visible govie-section-break--xl" />
-
-            <div>
-              <h2 className="govie-heading-m">
-                {t("sections.accessibility.title")}
-              </h2>
-              <p className="govie-body">
-                {t("sections.accessibility.description")}
-              </p>
-            </div>
-            <div>
-              <h2 className="govie-heading-m">{t("sections.support.title")}</h2>
-              <p className="govie-body">{t("sections.support.description")}</p>
-            </div>
-
-            <hr className="govie-section-break govie-section-break--visible govie-section-break--xl" />
-
-            <div>
-              <h2 className="govie-heading-l">
-                {t("sections.getStarted.title")}
-              </h2>
-              <p className="govie-body">
-                {t("sections.getStarted.description")}
-              </p>
+          <div className="two-columns-layout">
+            <div className="column">
+              <h1 className="govie-heading-l">
+                {t("sections.main.serviceName")}
+              </h1>
+              <h2 className="govie-heading-m">{t("sections.main.title")}</h2>
+              <p className="govie-body">{t("sections.main.description")}</p>
               <a
                 href={`${process.env.NEXT_PUBLIC_FORMS_URL}${props.params.locale}/664b6de45f7c9800231daf22`}
               >
@@ -216,14 +68,117 @@ export default async (props: Props) => {
                   data-module="govie-button"
                   className="govie-button govie-button--primary"
                 >
-                  {t("sections.getStarted.cta")}
+                  {t("sections.main.cta")}
                 </button>
               </a>
             </div>
+            <div className="column">
+              <Image
+                src={hero}
+                alt={t("sections.main.title")}
+                layout="responsive"
+              />
+            </div>
           </div>
+
+          <hr className="govie-section-break govie-section-break--visible govie-section-break--xl" />
+
+          <h2 className="govie-heading-m">{t("benefits")}</h2>
+
+          <div className="two-columns-layout">
+            <div className="column">
+              <h3 className="govie-heading-s">
+                {t("sections.integration.title")}
+              </h3>
+              <p className="govie-body">
+                {t("sections.integration.description")}
+              </p>
+            </div>
+            <div className="column">
+              <Image
+                src={integration}
+                alt={t("sections.integration.title")}
+                layout="responsive"
+              />
+            </div>
+          </div>
+
+          <hr className="govie-section-break govie-section-break--visible govie-section-break--xl" />
+
+          <div className="two-columns-layout">
+            <div className="column">
+              <h3 className="govie-heading-s">
+                {t("sections.security.title")}
+              </h3>
+              <p className="govie-body">{t("sections.security.description")}</p>
+            </div>
+            <div className="column">
+              <Image
+                src={security}
+                alt={t("sections.security.title")}
+                layout="responsive"
+              />
+            </div>
+          </div>
+
+          <hr className="govie-section-break govie-section-break--visible govie-section-break--xl" />
+
+          <h2 className="govie-heading-m">{t("sections.interface.title")}</h2>
+          <p className="govie-body">{t("sections.interface.description")}</p>
+
+          <div className="two-columns-layout">
+            <div className="column">
+              <p className="govie-body govie-!-font-size-16">
+                <i>{t("sections.interface.citizenPOV")}:</i>
+              </p>
+              <Image
+                src={interfaceCitizen}
+                alt={t("sections.interface.citizenPOV")}
+                layout="responsive"
+              />
+            </div>
+            <div className="column">
+              <p className="govie-body govie-!-font-size-16">
+                <i>{t("sections.interface.publicServantPOV")}:</i>
+              </p>
+              <Image
+                src={interfacePublicServant}
+                alt={t("sections.interface.publicServantPOV")}
+                layout="responsive"
+              />
+            </div>
+          </div>
+
+          <hr className="govie-section-break govie-section-break--visible govie-section-break--xl" />
+
+          <h2 className="govie-heading-m">
+            {t("sections.accessibility.title")}
+          </h2>
+          <p className="govie-body">
+            {t("sections.accessibility.description")}
+          </p>
+
+          <h2 className="govie-heading-m">{t("sections.support.title")}</h2>
+          <p className="govie-body">{t("sections.support.description")}</p>
+
+          <hr className="govie-section-break govie-section-break--visible govie-section-break--xl" />
+
+          <h2 className="govie-heading-m">{t("sections.getStarted.title")}</h2>
+          <p className="govie-body">{t("sections.getStarted.description")}</p>
+          <a
+            href={`${process.env.NEXT_PUBLIC_FORMS_URL}${props.params.locale}/664b6de45f7c9800231daf22`}
+          >
+            <button
+              id="button"
+              data-module="govie-button"
+              className="govie-button govie-button--primary"
+            >
+              {t("sections.getStarted.cta")}
+            </button>
+          </a>
         </div>
 
-        <hr className="govie-section-break govie-section-break govie-section-break--xl" />
+        <hr className="govie-section-break govie-section-break--xl" />
 
         <Footer />
       </body>
