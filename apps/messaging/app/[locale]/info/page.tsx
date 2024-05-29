3884ac1edf0a473186de0e308c039a9d6ad8056d
Norbert Nagy <nagy.norbert1989@gmail.com>
2024-05-29 15:57:40 +0300

    17270 messaging landing page (#348)
    * Messaging landing page
    
    * Update
    
    * Update

--- apps/messaging/app/[locale]/info/page.tsx
+++ apps/messaging/app/[locale]/info/page.tsx
@@ -6,14 +6,13 @@ import Footer from "./Footer";
 import { getTranslations } from "next-intl/server";
 import Image from "next/image";
 import hero from "../../../public/landingPage/hero.png";
-import integration from "../../../public/landingPage/integration.png";
-import interfaceCitizen from "../../../public/landingPage/interfaceCitizen.png";
-import interfacePublicServant from "../../../public/landingPage/interfacePublicServant.png";
-import security from "../../../public/landingPage/security.png";
+import multiChannel from "../../../public/landingPage/multi_channel.png";
+import template from "../../../public/landingPage/template.png";
+import postbox from "../../../public/landingPage/postbox.png";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Payments",
+  title: "Messaging",
 };
 
 type Props = {
@@ -22,25 +21,42 @@ type Props = {
   };
 };
 
+const availableLinks = {
+  DEV: {
+    learnMoreForm:
+      "https://www.formsg.testing.gov.ie/en/664b6de45f7c9800231daf22",
+    feedbackLink:
+      "https://www.formsg.testing.gov.ie/en/664c61ba5f7c9800231db294",
+  },
+  STA: {
+    learnMoreForm:
+      "https://www.formsg.testing.gov.ie/en/664b6de45f7c9800231daf22",
+    feedbackLink:
+      "https://www.formsg.testing.gov.ie/en/664c61ba5f7c9800231db294",
+  },
+};
+
 export default async (props: Props) => {
   const t = await getTranslations("LandingPage");
+  const tBanner = await getTranslations("AlphaBanner");
+  //Let's hardcode Dev for now, in a separate PR - we will add an env var to handle that
+  const environment = "DEV";
+  const links = availableLinks[environment];
 
   return (
     <>
       <Header />
+
       <div className="govie-width-container">
         <div className="govie-phase-banner">
           <p className="govie-phase-banner__content">
             <strong className="govie-tag govie-phase-banner__content__tag">
-              {t("AlphaBanner.tag")}
+              {tBanner("tag")}
             </strong>
             <span className="govie-phase-banner__text">
-              {t.rich("AlphaBanner.bannerText", {
+              {tBanner.rich("bannerText", {
                 anchor: (chunks) => (
-                  <a
-                    className="govie-link"
-                    href={`${process.env.NEXT_PUBLIC_FORMS_URL}${props.params.locale}/664c61ba5f7c9800231db294`}
-                  >
+                  <a className="govie-link" href={links.feedbackLink}>
                     {chunks}
                   </a>
                 ),
@@ -57,17 +73,6 @@ export default async (props: Props) => {
               {t("sections.main.title")}
             </h1>
             <p className="govie-body">{t("sections.main.description")}</p>
-            <a
-              href={`${process.env.NEXT_PUBLIC_FORMS_URL}${props.params.locale}/664b6de45f7c9800231daf22`}
-            >
-              <button
-                id="button"
-                data-module="govie-button"
-                className="govie-button govie-button--primary"
-              >
-                {t("sections.main.cta")}
-              </button>
-            </a>
           </div>
           <div className="column">
             <Image
@@ -80,21 +85,50 @@ export default async (props: Props) => {
 
         <hr className="govie-section-break govie-section-break--visible govie-section-break--xl" />
 
-        <h2 className="govie-heading-m">{t("benefits")}</h2>
+        <h2 className="govie-heading-m">{t("sections.benefits.title")}</h2>
 
         <div className="two-columns-layout">
           <div className="column">
             <h3 className="govie-heading-s">
-              {t("sections.integration.title")}
+              {t("sections.benefits.section1.title")}
             </h3>
             <p className="govie-body">
-              {t("sections.integration.description")}
+              {t("sections.benefits.section1.description")}
             </p>
           </div>
           <div className="column">
             <Image
-              src={integration}
-              alt={t("sections.integration.title")}
+              src={multiChannel}
+              alt={t("sections.benefits.section1.title")}
+              layout="responsive"
+            />
+          </div>
+        </div>
+
+        <hr className="govie-section-break govie-section-break--m" />
+
+        <h3 className="govie-heading-s">
+          {t("sections.benefits.section2.title")}
+        </h3>
+        <p className="govie-body">
+          {t("sections.benefits.section2.description")}
+        </p>
+
+        <hr className="govie-section-break govie-section-break--m" />
+
+        <div className="two-columns-layout">
+          <div className="column">
+            <h3 className="govie-heading-s">
+              {t("sections.benefits.section3.title")}
+            </h3>
+            <p className="govie-body">
+              {t("sections.benefits.section3.description")}
+            </p>
+          </div>
+          <div className="column">
+            <Image
+              src={template}
+              alt={t("sections.benefits.section2.title")}
               layout="responsive"
             />
           </div>
@@ -104,13 +138,17 @@ export default async (props: Props) => {
 
         <div className="two-columns-layout">
           <div className="column">
-            <h3 className="govie-heading-s">{t("sections.security.title")}</h3>
-            <p className="govie-body">{t("sections.security.description")}</p>
+            <h3 className="govie-heading-s">
+              {t("sections.benefits.section4.title")}
+            </h3>
+            <p className="govie-body">
+              {t("sections.benefits.section4.description")}
+            </p>
           </div>
           <div className="column">
             <Image
-              src={security}
-              alt={t("sections.security.title")}
+              src={postbox}
+              alt={t("sections.benefits.section3.title")}
               layout="responsive"
             />
           </div>
@@ -118,53 +156,46 @@ export default async (props: Props) => {
 
         <hr className="govie-section-break govie-section-break--m" />
 
-        <h2 className="govie-heading-m">{t("sections.interface.title")}</h2>
-        <p className="govie-body">{t("sections.interface.description")}</p>
-
-        <div className="two-columns-layout">
-          <div className="column">
-            <p className="govie-body govie-!-font-size-16">
-              <i>{t("sections.interface.citizenPOV")}:</i>
-            </p>
-            <Image
-              src={interfaceCitizen}
-              alt={t("sections.interface.citizenPOV")}
-              layout="responsive"
-            />
-          </div>
-          <div className="column">
-            <p className="govie-body govie-!-font-size-16">
-              <i>{t("sections.interface.publicServantPOV")}:</i>
-            </p>
-            <Image
-              src={interfacePublicServant}
-              alt={t("sections.interface.publicServantPOV")}
-              layout="responsive"
-            />
-          </div>
-        </div>
+        <h3 className="govie-heading-s">
+          {t("sections.benefits.section5.title")}
+        </h3>
+        <p className="govie-body">
+          {t("sections.benefits.section5.description")}
+        </p>
 
         <hr className="govie-section-break govie-section-break--m" />
 
-        <h2 className="govie-heading-m">{t("sections.accessibility.title")}</h2>
-        <p className="govie-body">{t("sections.accessibility.description")}</p>
-
-        <h2 className="govie-heading-m">{t("sections.support.title")}</h2>
-        <p className="govie-body">{t("sections.support.description")}</p>
+        <h3 className="govie-heading-s">
+          {t("sections.benefits.section6.title")}
+        </h3>
+        <p className="govie-body">
+          {t("sections.benefits.section6.description")}
+        </p>
 
         <hr className="govie-section-break govie-section-break--visible govie-section-break--xl" />
 
         <h2 className="govie-heading-m">{t("sections.getStarted.title")}</h2>
         <p className="govie-body">{t("sections.getStarted.description")}</p>
-        <a
-          href={`${process.env.NEXT_PUBLIC_FORMS_URL}${props.params.locale}/664b6de45f7c9800231daf22`}
-        >
+        <a href={links.learnMoreForm}>
           <button
             id="button"
             data-module="govie-button"
             className="govie-button govie-button--primary"
           >
             {t("sections.getStarted.cta")}
+            <svg
+              className="govie-button__icon-right"
+              width="16"
+              height="17"
+              viewBox="0 0 16 17"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M8 0.5L6.59 1.91L12.17 7.5H0V9.5H12.17L6.59 15.09L8 16.5L16 8.5L8 0.5Z"
+                fill="white"
+              ></path>
+            </svg>
           </button>
         </a>
       </div>
