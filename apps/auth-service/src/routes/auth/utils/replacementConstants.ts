018cef1670d9b49357c7f1fc4dfec7233fdf9005
Francesco Maida <francesco.maida@nearform.com>
2024-06-03 17:31:23 +0200

    feat(AuthService) redirect to subpath after login (#519)

--- apps/auth-service/src/routes/auth/utils/replacementConstants.ts
+++ apps/auth-service/src/routes/auth/utils/replacementConstants.ts
@@ -1,5 +1,6 @@
 export const SESSION_ID = "%SESSION_ID%";
-export const REDIRECT_URL = "%REDIRECT_URL%";
+export const REDIRECT_HOST = "%REDIRECT_HOST%";
+export const REDIRECT_PATH = "%REDIRECT_PATH%";
 export const REDIRECT_TIMEOUT = "%REDIRECT_TIMEOUT%";
 export const CLIENT_ID = "%CLIENT_ID%";
 export const CALLBACK_URL = "%CALLBACK_URL%";
