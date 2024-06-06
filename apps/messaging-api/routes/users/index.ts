0aaf793604f52f52b1a93f7e1b777b4da7783f7e
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-06-06 16:48:41 +0200

    Updated sdks

--- apps/messaging-api/routes/users/index.ts
+++ apps/messaging-api/routes/users/index.ts
@@ -105,110 +105,6 @@ export default async function users(app: FastifyInstance) {
     },
   );
 
-  app.get<{
-    Params: { organisationId: string };
-    Response: { data: UserInvitation };
-  }>(
-    "/invitations/:organisationId",
-    {
-      preValidation: app.verifyUser,
-      schema: {
-        tags,
-        params: Type.Object({
-          organisationId: Type.String({ format: "uuid" }),
-        }),
-        response: {
-          200: Type.Object({ data: UserInvitationSchema }),
-          400: HttpError,
-          404: HttpError,
-          500: HttpError,
-        },
-      },
-    },
-    async (
-      request: FastifyRequest<{
-        Params: { organisationId: string };
-        Response: { data: UserInvitation };
-      }>,
-      _reply: FastifyReply,
-    ) => ({
-      data: await getInvitationForUser({
-        userProfileId: request.user!.id,
-        organisationId: request.params.organisationId,
-        pg: app.pg,
-      }),
-    }),
-  );
-
-  interface PatchOrgInvitationSchema {
-    Params: { organisationId: string };
-    Body: OrganisationInvitationFeedback;
-    Response: { data: UserInvitation };
-  }
-
-  app.patch<PatchOrgInvitationSchema>(
-    "/invitations/:organisationId",
-    {
-      preValidation: app.verifyUser,
-      schema: {
-        tags,
-        body: OrganisationInvitationFeedbackSchema,
-        params: Type.Object({
-          organisationId: Type.String({ format: "uuid" }),
-        }),
-        response: {
-          202: Type.Object({ data: UserInvitationSchema }),
-          400: HttpError,
-          404: HttpError,
-          500: HttpError,
-        },
-      },
-    },
-    async (
-      request: FastifyRequest<PatchOrgInvitationSchema>,
-      _reply: FastifyReply,
-    ) => ({
-      data: await updateOrganisationFeedback({
-        userProfileId: request.user!.id,
-        organisationId: request.params.organisationId,
-        pg: app.pg,
-        feedback: request.body,
-      }),
-    }),
-  );
-
-  interface PatchInvitationSchema {
-    Body: InvitationFeedback;
-    Response: { data: User };
-  }
-
-  app.patch<PatchInvitationSchema>(
-    "/invitations",
-    {
-      preValidation: app.verifyUser,
-      schema: {
-        tags,
-        body: InvitationFeedbackSchema,
-        response: {
-          202: Type.Object({ data: UserSchema }),
-          400: HttpError,
-          404: HttpError,
-          500: HttpError,
-        },
-      },
-    },
-    async (
-      request: FastifyRequest<PatchInvitationSchema>,
-      _reply: FastifyReply,
-    ) => ({
-      data: await updateInvitationStatus({
-        userProfileId: request.user!.id,
-        pg: app.pg,
-        feedback: request.body,
-      }),
-    }),
-  );
-
   interface GetImportsSchema {
     Querystring: { organisationId?: string };
     Response: { data: Omit<UsersImport, "usersData">[] };
@@ -353,4 +249,108 @@ export default async function users(app: FastifyInstance) {
 
     return organisationId;
   };
+
+  app.get<{
+    Params: { organisationId: string };
+    Response: { data: UserInvitation };
+  }>(
+    "/invitations/:organisationId",
+    {
+      preValidation: app.verifyUser,
+      schema: {
+        tags,
+        params: Type.Object({
+          organisationId: Type.String({ format: "uuid" }),
+        }),
+        response: {
+          200: Type.Object({ data: UserInvitationSchema }),
+          400: HttpError,
+          404: HttpError,
+          500: HttpError,
+        },
+      },
+    },
+    async (
+      request: FastifyRequest<{
+        Params: { organisationId: string };
+        Response: { data: UserInvitation };
+      }>,
+      _reply: FastifyReply,
+    ) => ({
+      data: await getInvitationForUser({
+        userProfileId: request.user!.id,
+        organisationId: request.params.organisationId,
+        pg: app.pg,
+      }),
+    }),
+  );
+
+  interface PatchOrgInvitationSchema {
+    Params: { organisationId: string };
+    Body: OrganisationInvitationFeedback;
+    Response: { data: UserInvitation };
+  }
+
+  app.patch<PatchOrgInvitationSchema>(
+    "/invitations/:organisationId",
+    {
+      preValidation: app.verifyUser,
+      schema: {
+        tags,
+        body: OrganisationInvitationFeedbackSchema,
+        params: Type.Object({
+          organisationId: Type.String({ format: "uuid" }),
+        }),
+        response: {
+          202: Type.Object({ data: UserInvitationSchema }),
+          400: HttpError,
+          404: HttpError,
+          500: HttpError,
+        },
+      },
+    },
+    async (
+      request: FastifyRequest<PatchOrgInvitationSchema>,
+      _reply: FastifyReply,
+    ) => ({
+      data: await updateOrganisationFeedback({
+        userProfileId: request.user!.id,
+        organisationId: request.params.organisationId,
+        pg: app.pg,
+        feedback: request.body,
+      }),
+    }),
+  );
+
+  interface PatchInvitationSchema {
+    Body: InvitationFeedback;
+    Response: { data: User };
+  }
+
+  app.patch<PatchInvitationSchema>(
+    "/invitations",
+    {
+      preValidation: app.verifyUser,
+      schema: {
+        tags,
+        body: InvitationFeedbackSchema,
+        response: {
+          202: Type.Object({ data: UserSchema }),
+          400: HttpError,
+          404: HttpError,
+          500: HttpError,
+        },
+      },
+    },
+    async (
+      request: FastifyRequest<PatchInvitationSchema>,
+      _reply: FastifyReply,
+    ) => ({
+      data: await updateInvitationStatus({
+        userProfileId: request.user!.id,
+        pg: app.pg,
+        feedback: request.body,
+      }),
+    }),
+  );
 }
