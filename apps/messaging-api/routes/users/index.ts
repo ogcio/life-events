01589d32a1baf4af8f6ce6f59141d3879234aca8
SamSalvatico <40636569+SamSalvatico@users.noreply.github.com>
2024-06-06 16:55:32 +0200

    Feature/add-imports-apis-to-sdk (#545)
    * feat(messaging): fixed routes
    
    * feat(messaging): updated route to get imports
    
    * Added read users import
    
    * Fixed get import routes
    
    * Added get user invitations query
    
    * Added import user csv
    
    * Updated sdks
    
    * Updated sdks

--- apps/messaging-api/routes/users/index.ts
+++ apps/messaging-api/routes/users/index.ts
@@ -18,6 +18,8 @@ import {
   InvitationFeedback,
   InvitationFeedbackSchema,
   UserSchema,
+  UsersImportSchema,
+  UsersImport,
 } from "../../types/usersSchemaDefinitions";
 import {
   getInvitationForUser,
@@ -25,12 +27,18 @@ import {
   updateOrganisationFeedback,
 } from "../../services/users/invitations/accept-invitations";
 import { createError } from "@fastify/error";
+import {
+  READ_USER_IMPORTS_ERROR,
+  getUserImportForOrganisation,
+  getUserImportsForOrganisation,
+  getUserInvitationsForImport,
+} from "../../services/users/import/read-user-imports";
 
 const tags = ["Users"];
 
 export default async function users(app: FastifyInstance) {
   app.post(
-    "/import/csv",
+    "/imports/csv",
     {
       preValidation: app.verifyUser,
       schema: {
@@ -40,6 +48,7 @@ export default async function users(app: FastifyInstance) {
           "5xx": HttpError,
           "4xx": HttpError,
         },
+        consumes: ["multipart/form-data"],
       },
     },
     async (request: FastifyRequest, _reply: FastifyReply) => {
@@ -55,7 +64,7 @@ export default async function users(app: FastifyInstance) {
   );
 
   app.get(
-    "/import/csv/template",
+    "/imports/csv/template",
     {
       preValidation: app.verifyUser,
       schema: {
@@ -73,7 +82,7 @@ export default async function users(app: FastifyInstance) {
   );
 
   app.post<{ Body: CsvRecord[] }>(
-    "/import",
+    "/imports",
     {
       preValidation: app.verifyUser,
       schema: {
@@ -95,6 +104,151 @@ export default async function users(app: FastifyInstance) {
       });
     },
   );
+
+  interface GetImportsSchema {
+    Querystring: { organisationId?: string };
+    Response: { data: Omit<UsersImport, "usersData">[] };
+  }
+
+  app.get<GetImportsSchema>(
+    "/imports",
+    {
+      preValidation: app.verifyUser,
+      schema: {
+        tags,
+        querystring: Type.Optional(
+          Type.Object({
+            organisationId: Type.Optional(Type.String({ format: "uuid" })),
+          }),
+        ),
+        response: {
+          200: Type.Object({
+            data: Type.Array(Type.Omit(UsersImportSchema, ["usersData"])),
+          }),
+          "5xx": HttpError,
+          "4xx": HttpError,
+        },
+      },
+    },
+    async (
+      request: FastifyRequest<GetImportsSchema>,
+      _reply: FastifyReply,
+    ) => ({
+      data: await getUserImportsForOrganisation({
+        logger: request.log,
+        pool: app.pg.pool,
+        organisationId: getOrganisationIdFromRequest(request),
+      }),
+    }),
+  );
+
+  interface GetImportSchema {
+    Querystring: { organisationId?: string };
+    Response: { data: UsersImport };
+    Params: { importId: string };
+  }
+  app.get<GetImportSchema>(
+    "/imports/:importId",
+    {
+      preValidation: app.verifyUser,
+      schema: {
+        tags,
+        querystring: Type.Optional(
+          Type.Object({
+            organisationId: Type.Optional(Type.String({ format: "uuid" })),
+          }),
+        ),
+        params: Type.Object({ importId: Type.String({ format: "uuid" }) }),
+        response: {
+          200: Type.Object({
+            data: UsersImportSchema,
+          }),
+          "5xx": HttpError,
+          "4xx": HttpError,
+        },
+      },
+    },
+    async (request: FastifyRequest<GetImportSchema>, _reply: FastifyReply) => ({
+      data: await getUserImportForOrganisation({
+        logger: request.log,
+        pool: app.pg.pool,
+        organisationId: getOrganisationIdFromRequest(request),
+        importId: request.params.importId,
+      }),
+    }),
+  );
+
+  interface GetUserInvitationsSchema {
+    Querystring: { organisationId?: string };
+    Response: { data: UserInvitation[] };
+    Params: { importId: string };
+  }
+  app.get<GetUserInvitationsSchema>(
+    "/imports/:importId/users",
+    {
+      preValidation: app.verifyUser,
+      schema: {
+        tags,
+        querystring: Type.Optional(
+          Type.Object({
+            organisationId: Type.Optional(Type.String({ format: "uuid" })),
+          }),
+        ),
+        params: Type.Object({ importId: Type.String({ format: "uuid" }) }),
+        response: {
+          200: Type.Object({
+            data: Type.Array(UserInvitationSchema),
+          }),
+          "5xx": HttpError,
+          "4xx": HttpError,
+        },
+      },
+    },
+    async (
+      request: FastifyRequest<GetUserInvitationsSchema>,
+      _reply: FastifyReply,
+    ) => ({
+      data: await getUserInvitationsForImport({
+        logger: request.log,
+        pool: app.pg.pool,
+        organisationId: getOrganisationIdFromRequest(request),
+        importId: request.params.importId,
+      }),
+    }),
+  );
+
+  const saveRequestFile = async (request: FastifyRequest): Promise<string> => {
+    const file = await request.files();
+    if (!file) {
+      throw createError(
+        IMPORT_USERS_ERROR,
+        "File is missing in the request",
+        400,
+      )();
+    }
+
+    const savedFiles = await request.saveRequestFiles();
+
+    return savedFiles[0].filepath;
+  };
+
+  const getOrganisationIdFromRequest = (request: FastifyRequest): string => {
+    const query = request.query as { organisationId?: string };
+    // organisationId query parameter added to
+    // make us able to test it until we won't have
+    // an organisation id set into the logged in user
+    const organisationId =
+      request.user!.organisation_id ?? query.organisationId;
+    if (!organisationId) {
+      throw createError(
+        READ_USER_IMPORTS_ERROR,
+        "Cannot retrieve an organisation id",
+        400,
+      )();
+    }
+
+    return organisationId;
+  };
 
   app.get<{
     Params: { organisationId: string };
@@ -199,19 +353,4 @@ export default async function users(app: FastifyInstance) {
       }),
     }),
   );
-
-  const saveRequestFile = async (request: FastifyRequest): Promise<string> => {
-    const file = await request.files();
-    if (!file) {
-      throw createError(
-        IMPORT_USERS_ERROR,
-        "File is missing in the request",
-        400,
-      )();
-    }
-
-    const savedFiles = await request.saveRequestFiles();
-
-    return savedFiles[0].filepath;
-  };
 }
--- apps/messaging-api/services/users/shared-users.ts
+++ apps/messaging-api/services/users/shared-users.ts
@@ -1,7 +1,11 @@
 import { createError } from "@fastify/error";
 import { PoolClient, QueryResult } from "pg";
 import { isNativeError } from "util/types";
-import { User } from "../../types/usersSchemaDefinitions";
+import {
+  User,
+  UserInvitation,
+  UsersImport,
+} from "../../types/usersSchemaDefinitions";
 
 const getUser = async (params: {
   client: PoolClient;
@@ -94,4 +98,98 @@ export const getUserByContacts = async (params: {
     errorCode: params.errorCode,
     logicalWhereOperator: "OR",
   });
+};
+
+export const getUserImports = async (params: {
+  client: PoolClient;
+  whereClauses: string[];
+  whereValues: string[];
+  errorCode: string;
+  logicalWhereOperator?: string;
+  limit?: number;
+  includeUsersData: boolean;
+}): Promise<UsersImport[]> => {
+  try {
+    const usersDataClause = params.includeUsersData
+      ? 'users_data as "usersData",'
+      : "";
+    const limitClause = params.limit ? `LIMIT ${params.limit}` : "";
+    const operator = params.logicalWhereOperator
+      ? ` ${params.logicalWhereOperator} `
+      : " AND ";
+    const result = await params.client.query<UsersImport>(
+      `
+        SELECT 
+            organisation_id as "organisationId",
+            imported_at as "importedAt",
+            ${usersDataClause}
+            import_channel as "importChannel",
+            retry_count as "retryCount",
+            last_retry_at as "lastRetryAt",
+            import_id as "importId"
+        FROM users_imports where ${params.whereClauses.join(operator)} ${limitClause}
+      `,
+      params.whereValues,
+    );
+
+    return result.rows;
+  } catch (error) {
+    const message = isNativeError(error) ? error.message : "unknown error";
+    throw createError(
+      params.errorCode,
+      `Error retrieving user imports: ${message}`,
+      500,
+    )();
+  }
+};
+
+export const getUserInvitationsForOrganisation = async (params: {
+  client: PoolClient;
+  organisationId: string;
+  whereClauses: string[];
+  whereValues: string[];
+  errorCode: string;
+  logicalWhereOperator?: string;
+  limit?: number;
+}): Promise<UserInvitation[]> => {
+  try {
+    const limitClause = params.limit ? `LIMIT ${params.limit}` : "";
+    const organisationIndex = `$${params.whereValues.length + 1}`;
+    const operator = params.logicalWhereOperator
+      ? ` ${params.logicalWhereOperator} `
+      : " AND ";
+
+    const result = await params.client.query<UserInvitation>(
+      `
+        SELECT 
+            ouc.user_id as "id",
+                users.user_profile_id as "userProfileId",
+                users.email as "email",
+                users.phone as "phone",
+                users.details as "details",
+                ouc.organisation_id as "organisationId",
+                ouc.invitation_status as "organisationInvitationStatus",
+                ouc.invitation_sent_at  as "organisationInvitationSentAt",
+                ouc.invitation_feedback_at as "organisationInvitationFeedbackAt",
+                ouc.preferred_transports as "organisationPreferredTransports",
+                users.correlation_quality as "correlationQuality",
+                users.user_status as "userStatus"
+            from users
+            left join organisation_user_configurations ouc on ouc.user_id = users.id 
+            	and ouc.organisation_id = ${organisationIndex}
+            left join users_imports on users_imports.organisation_id = ouc.organisation_id 
+            where ${params.whereClauses.join(operator)} ${limitClause}
+      `,
+      [...params.whereValues, params.organisationId],
+    );
+
+    return result.rows;
+  } catch (error) {
+    const message = isNativeError(error) ? error.message : "unknown error";
+    throw createError(
+      params.errorCode,
+      `Error retrieving user invitations: ${message}`,
+      500,
+    )();
+  }
 };
