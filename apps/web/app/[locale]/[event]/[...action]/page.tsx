4b4a9b0819c846eb4e293253e5f187b7c97cb24a
Tiago Ramos <relvao@gmail.com>
2024-06-10 08:59:16 +0100

    protect pages under feature flags

--- apps/web/app/[locale]/[event]/[...action]/page.tsx
+++ apps/web/app/[locale]/[event]/[...action]/page.tsx
@@ -6,24 +6,31 @@ import OrderBirthCertificate from "./OrderBirthCertificate/OrderBirthCertificate
 import NotifyDeath from "./NotifyDeath/NotifyDeath";
 import ApplyJobseekersAllowance from "./ApplyJobseekersAllowance/ApplyJobseekersAllowance";
 import GetDigitalWallet from "./GetDigitalWallet/GetDigitalWallet";
+import { isFeatureFlagEnabled } from "feature-flags/utils";
 
-const componentsMap = {
-  [routes.driving.renewDriversLicence.slug]: RenewDriversLicence,
-  [routes.health.orderEHIC.slug]: OrderEHIC,
-  [routes.birth.orderBirthCertificate.slug]: OrderBirthCertificate,
-  [routes.death.notifyDeath.slug]: NotifyDeath,
-  [routes.employment.applyJobseekersAllowance.slug]: ApplyJobseekersAllowance,
-  [routes.digitalWallet.getDigitalWallet.slug]: GetDigitalWallet,
-};
+const componentsMap = async () => ({
+  [routes.driving.renewDriversLicence.slug]:
+    (await isFeatureFlagEnabled("driving")) && RenewDriversLicence,
+  [routes.health.orderEHIC.slug]:
+    (await isFeatureFlagEnabled("health")) && OrderEHIC,
+  [routes.birth.orderBirthCertificate.slug]:
+    (await isFeatureFlagEnabled("birth")) && OrderBirthCertificate,
+  [routes.death.notifyDeath.slug]:
+    (await isFeatureFlagEnabled("death")) && NotifyDeath,
+  [routes.employment.applyJobseekersAllowance.slug]:
+    (await isFeatureFlagEnabled("employment")) && ApplyJobseekersAllowance,
+  [routes.digitalWallet.getDigitalWallet.slug]:
+    (await isFeatureFlagEnabled("digitalWallet")) && GetDigitalWallet,
+});
 
-export default function ActionPage(props: web.NextPageProps) {
+export default async function ActionPage(props: web.NextPageProps) {
   const path = props.params.action.at(0);
 
   if (!path) {
     throw notFound();
   }
 
-  const Component = componentsMap[path];
+  const Component = (await componentsMap())[path];
 
   if (Component) {
     return (
