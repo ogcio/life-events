a2b2938d62aa9826a3d2d05c42c1b4ae1e04aa4e
Francesco Maida <francesco.maida@nearform.com>
2024-06-12 09:20:20 +0200

    fix(Onboarding) email validator

--- apps/web/app/utils/form.ts
+++ apps/web/app/utils/form.ts
@@ -141,7 +141,7 @@ export const validation = {
             errorValue: value || "",
           },
         ]
-      : !/^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value)
+      : !/^[a-zA-Z0-9._+\-`']+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value)
         ? [
             {
               field,
