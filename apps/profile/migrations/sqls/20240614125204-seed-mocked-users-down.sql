b881e6753e31d05e8d3e747bcf3aba30e05027c7
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-06-14 15:03:26 +0200

    chore(profile): seed users

---
+++ apps/profile/migrations/sqls/20240614125204-seed-mocked-users-down.sql
@@ -0,0 +1,5 @@
+DELETE FROM user_details
+WHERE 
+    (user_id = '239c2e19-6869-44ef-bdd2-eb17819cf51e' AND ppsn = 'BOB_RED_PPSN') OR
+    (user_id = '5eca4350-1d83-427a-849f-c2db698a5dfc' AND ppsn = 'ANNE_BROWN_PPSN') OR
+    (user_id = 'fc6f3dd9-79ff-424f-b41e-e1959e25b414' AND ppsn = 'MARGARET_BLUE_PPSN');
