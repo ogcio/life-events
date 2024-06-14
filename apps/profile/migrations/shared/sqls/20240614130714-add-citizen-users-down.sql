6097e391818c7fb32a2f3eee47792bc54cd52590
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-06-14 15:13:43 +0200

    Updated citizen users

---
+++ apps/profile/migrations/shared/sqls/20240614130714-add-citizen-users-down.sql
@@ -0,0 +1,5 @@
+DELETE FROM users
+WHERE
+    (id = '239c2e19-6869-44ef-bdd2-eb17819cf51e' AND govid_email = 'bob.red@profile.com') OR
+    (id = '5eca4350-1d83-427a-849f-c2db698a5dfc' AND govid_email = 'anne.brown@profile.com') OR
+    (id = 'fc6f3dd9-79ff-424f-b41e-e1959e25b414' AND govid_email = 'margaret.blue@profile.com')
