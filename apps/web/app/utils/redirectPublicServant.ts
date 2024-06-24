cc42a53b9d431e32424cd74118b6ddae8566aee0
Francesco Maida <francesco.maida@nearform.com>
2024-06-24 14:55:45 +0200

    minor cleanup

--- apps/web/app/utils/redirectPublicServant.ts
+++
@@ -1,9 +0,0 @@
-import { PgSessions } from "auth/sessions";
-import { RedirectType, redirect } from "next/navigation";
-
-export default async (locale: string) => {
-  const { publicServant } = await PgSessions.get();
-  if (publicServant) {
-    redirect(`/${locale}/admin`, RedirectType.replace);
-  }
-};
