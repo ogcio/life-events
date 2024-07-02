352b2387fa8a2479b6aade9fca794355bbfcbbd7
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-07-02 15:30:27 +0200

    feat(messaging): start updating logto integration config

--- apps/messaging/app/[locale]/logto_integration/config.ts
+++ apps/messaging/app/[locale]/logto_integration/config.ts
@@ -1,6 +1,8 @@
 import { AuthUserScope } from "auth/auth-session";
 
-export default {
+export const messagingApiResource = process.env.MESSAGES_BACKEND_URL + "/";
+
+export const baseConfig = {
   cookieSecure: process.env.NODE_ENV === "production",
   baseUrl: process.env.NEXT_PUBLIC_MESSAGING_SERVICE_ENTRY_POINT as string,
   endpoint: process.env.LOGTO_ENDPOINT as string,
@@ -8,8 +10,26 @@ export default {
 
   appId: process.env.LOGTO_MESSAGING_APP_ID as string,
   appSecret: process.env.LOGTO_MESSAGING_APP_SECRET as string,
+};
 
-  scopes: [AuthUserScope.Organizations, AuthUserScope.OrganizationRoles],
+const orgScopes = [
+  AuthUserScope.Organizations,
+  AuthUserScope.OrganizationRoles,
+];
+
+const citizenScopes = ["messaging:message.self:read"];
+const publicServantScopes = [
+  "messaging:message:*",
+  "messaging:provider:*",
+  "messaging:template:*",
+  "messaging:citizen:*",
+];
+
+export default {
+  ...baseConfig,
+  // All the available resources to the app
+  resources: [messagingApiResource],
+  scopes: [...orgScopes, ...citizenScopes, ...publicServantScopes],
 };
 
 export const postSignoutRedirect =
