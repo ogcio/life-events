e3375eb522dfd86adf45436a824041d9c733b002
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-07-08 16:05:05 +0200

    organisation id from 12 to 21

--- packages/messages/migrations/sqls/20240708125718-users-from-uuid-to-nanoid-up.sql
+++ packages/messages/migrations/sqls/20240708125718-users-from-uuid-to-nanoid-up.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 ALTER TABLE users_imports
-ALTER COLUMN organisation_id SET DATA TYPE varchar(12);
+ALTER COLUMN organisation_id SET DATA TYPE varchar(21);
 
 ALTER TABLE organisation_user_configurations
 DROP CONSTRAINT organisations_users_user_id_fkey;
@@ -12,10 +12,10 @@ DROP CONSTRAINT tags_users_user_id_fkey;
 ALTER TABLE users
 ALTER COLUMN user_profile_id SET DATA TYPE varchar(12),
 ALTER COLUMN id SET DATA TYPE varchar(12),
-ALTER COLUMN importer_organisation_id SET DATA TYPE varchar(12);
+ALTER COLUMN importer_organisation_id SET DATA TYPE varchar(21);
 
 ALTER TABLE email_providers
-ALTER COLUMN organisation_id SET DATA TYPE varchar(12);
+ALTER COLUMN organisation_id SET DATA TYPE varchar(21);
 
 ALTER TABLE form_errors
 ALTER COLUMN user_id SET DATA TYPE varchar(12);
@@ -28,25 +28,25 @@ ALTER COLUMN user_id SET DATA TYPE varchar(12);
 
 ALTER TABLE message_template_meta
 ALTER COLUMN created_by_user_id SET DATA TYPE varchar(12),
-ALTER COLUMN organisation_id SET DATA TYPE varchar(12);
+ALTER COLUMN organisation_id SET DATA TYPE varchar(21);
 
 ALTER TABLE message_template_states
 ALTER COLUMN user_id SET DATA TYPE varchar(12);
 
 ALTER TABLE messages
 ALTER COLUMN user_id SET DATA TYPE varchar(12),
-ALTER COLUMN organisation_id SET DATA TYPE varchar(12);
+ALTER COLUMN organisation_id SET DATA TYPE varchar(21);
 
 ALTER TABLE organisation_user_configurations
 ALTER COLUMN user_id SET DATA TYPE varchar(12),
-ALTER COLUMN organisation_id SET DATA TYPE varchar(12),
+ALTER COLUMN organisation_id SET DATA TYPE varchar(21),
 ADD CONSTRAINT organisations_users_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id);
 
 ALTER TABLE sms_provider_states
 ALTER COLUMN user_id SET DATA TYPE varchar(12);
 
 ALTER TABLE sms_providers
-ALTER COLUMN organisation_id SET DATA TYPE varchar(12);
+ALTER COLUMN organisation_id SET DATA TYPE varchar(21);
 
 ALTER TABLE tags_users
 ALTER COLUMN user_id SET DATA TYPE varchar(12),
