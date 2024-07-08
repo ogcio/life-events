82a79c33e92415a110d073d5bd42d037a5f63d7c
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-07-08 15:54:35 +0200

    text to varchar

--- apps/profile/migrations/sqls/20240708133834-from-uuid-to-text-up.sql
+++ apps/profile/migrations/sqls/20240708133834-from-uuid-to-text-up.sql
@@ -7,17 +7,17 @@ ALTER TABLE user_entitlements
 DROP CONSTRAINT entitlements_user_id_fkey;
 
 ALTER TABLE user_details
-ALTER COLUMN user_id SET DATA TYPE text;
+ALTER COLUMN user_id SET DATA TYPE varchar(12);
 
 ALTER TABLE user_entitlements
-ALTER COLUMN user_id SET DATA TYPE text,
+ALTER COLUMN user_id SET DATA TYPE varchar(12),
 ADD CONSTRAINT entitlements_user_id_fkey FOREIGN KEY (user_id) REFERENCES user_details (user_id);
 
 ALTER TABLE form_errors
-ALTER COLUMN user_id SET DATA TYPE text;
+ALTER COLUMN user_id SET DATA TYPE varchar(12);
 
 ALTER TABLE user_addresses
-ALTER COLUMN user_id SET DATA TYPE text,
+ALTER COLUMN user_id SET DATA TYPE varchar(12),
 ADD CONSTRAINT user_addresses_user_id_fkey FOREIGN KEY (user_id) REFERENCES user_details (user_id);
 
 COMMIT;
--- packages/messages/migrations/sqls/20240708125718-users-from-uuid-to-nanoid-up.sql
+++ packages/messages/migrations/sqls/20240708125718-users-from-uuid-to-nanoid-up.sql
@@ -1,23 +1,55 @@
 BEGIN;
 
-ALTER TABLE user_addresses
-DROP CONSTRAINT user_addresses_user_id_fkey;
+ALTER TABLE users_imports
+ALTER COLUMN organisation_id SET DATA TYPE varchar(12);
 
-ALTER TABLE user_entitlements
-DROP CONSTRAINT entitlements_user_id_fkey;
+ALTER TABLE organisation_user_configurations
+DROP CONSTRAINT organisations_users_user_id_fkey;
 
-ALTER TABLE user_details
-ALTER COLUMN user_id SET DATA TYPE uuid USING user_id:uuid;
+ALTER TABLE tags_users
+DROP CONSTRAINT tags_users_user_id_fkey;
 
-ALTER TABLE user_entitlements
-ALTER COLUMN user_id SET DATA TYPE uuid USING user_id:uuid,
-ADD CONSTRAINT entitlements_user_id_fkey FOREIGN KEY (user_id) REFERENCES user_details (user_id);
+ALTER TABLE users
+ALTER COLUMN user_profile_id SET DATA TYPE varchar(12),
+ALTER COLUMN id SET DATA TYPE varchar(12),
+ALTER COLUMN importer_organisation_id SET DATA TYPE varchar(12);
+
+ALTER TABLE email_providers
+ALTER COLUMN organisation_id SET DATA TYPE varchar(12);
 
 ALTER TABLE form_errors
-ALTER COLUMN user_id SET DATA TYPE uuid USING user_id:uuid;
+ALTER COLUMN user_id SET DATA TYPE varchar(12);
 
-ALTER TABLE user_addresses
-ALTER COLUMN user_id SET DATA TYPE uuid USING user_id:uuid,
-ADD CONSTRAINT user_addresses_user_id_fkey FOREIGN KEY (user_id) REFERENCES user_details (user_id);
+ALTER TABLE jobs
+ALTER COLUMN user_id SET DATA TYPE varchar(12);
+
+ALTER TABLE message_states
+ALTER COLUMN user_id SET DATA TYPE varchar(12);
+
+ALTER TABLE message_template_meta
+ALTER COLUMN created_by_user_id SET DATA TYPE varchar(12),
+ALTER COLUMN organisation_id SET DATA TYPE varchar(12);
+
+ALTER TABLE message_template_states
+ALTER COLUMN user_id SET DATA TYPE varchar(12);
+
+ALTER TABLE messages
+ALTER COLUMN user_id SET DATA TYPE varchar(12),
+ALTER COLUMN organisation_id SET DATA TYPE varchar(12);
+
+ALTER TABLE organisation_user_configurations
+ALTER COLUMN user_id SET DATA TYPE varchar(12),
+ALTER COLUMN organisation_id SET DATA TYPE varchar(12),
+ADD CONSTRAINT organisations_users_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id);
+
+ALTER TABLE sms_provider_states
+ALTER COLUMN user_id SET DATA TYPE varchar(12);
+
+ALTER TABLE sms_providers
+ALTER COLUMN organisation_id SET DATA TYPE varchar(12);
+
+ALTER TABLE tags_users
+ALTER COLUMN user_id SET DATA TYPE varchar(12),
+ADD CONSTRAINT tags_users_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id);
 
 COMMIT;
