a32012d33f5f37e99b35f1627c3e1cb5bdae9665
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-07-08 15:46:49 +0200

    chore(profile): from uuid to text

--- apps/profile/migrations/sqls/20240708133834-from-uuid-to-text-up.sql
+++ apps/profile/migrations/sqls/20240708133834-from-uuid-to-text-up.sql
@@ -7,17 +7,17 @@ ALTER TABLE user_entitlements
 DROP CONSTRAINT entitlements_user_id_fkey;
 
 ALTER TABLE user_details
-ALTER COLUMN user_id SET DATA TYPE uuid USING user_id:uuid;
+ALTER COLUMN user_id SET DATA TYPE text;
 
 ALTER TABLE user_entitlements
-ALTER COLUMN user_id SET DATA TYPE uuid USING user_id:uuid,
+ALTER COLUMN user_id SET DATA TYPE text,
 ADD CONSTRAINT entitlements_user_id_fkey FOREIGN KEY (user_id) REFERENCES user_details (user_id);
 
 ALTER TABLE form_errors
-ALTER COLUMN user_id SET DATA TYPE uuid USING user_id:uuid;
+ALTER COLUMN user_id SET DATA TYPE text;
 
 ALTER TABLE user_addresses
-ALTER COLUMN user_id SET DATA TYPE uuid USING user_id:uuid,
+ALTER COLUMN user_id SET DATA TYPE text,
 ADD CONSTRAINT user_addresses_user_id_fkey FOREIGN KEY (user_id) REFERENCES user_details (user_id);
 
 COMMIT;
