595292711da60b3f968e8155a1f373c361494d85
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-07-09 10:28:03 +0200

    removed useless insert

--- apps/profile/migrations/sqls/20240614125204-seed-mocked-users-up.sql
+++ apps/profile/migrations/sqls/20240614125204-seed-mocked-users-up.sql
@@ -5,8 +5,3 @@ VALUES
     ('5eca4350-1d83-427a-849f-c2db698a5dfc', 'Ms', 'Anne', 'Brown', '1990-01-01', 'ANNE_BROWN_PPSN', 'female', '2345432', 'anne.brown@profile.com', now(), now(), false, true),
     ('fc6f3dd9-79ff-424f-b41e-e1959e25b414', 'Mrs', 'Margaret', 'Blue', '1990-10-10', 'MARGARET_BLUE_PPSN', 'female', '00000000', 'margaret.blue@profile.com', now(), now(), false, true)
 ON CONFLICT DO NOTHING;
-
-
-INSERT INTO public.user_entitlements
-(id, "type", issue_date, expiry_date, document_number, user_id, firstname, lastname, created_at, updated_at)
-VALUES(gen_random_uuid(), 'fde', now(), now(), '123', 'fc6f3dd9-79ff-424f-b41e-e1959e25b414', 'aaa', 'aaa', now(), now()); 
