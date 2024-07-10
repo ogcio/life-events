bc1a91ae2914e40d41e5a6f741ae9f01f15e6e2b
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-07-10 12:12:21 +0200

    added common logger

--- packages/nextjs-logging-wrapper/src/index.ts
+++ packages/nextjs-logging-wrapper/src/index.ts
@@ -1,5 +1,7 @@
 import { getClientLogger as clientLogger } from "./client-logger.js";
 import { getServerLogger as serverLogger } from "./server-logger.js";
+import { getCommonLogger as commonLogger } from "./common-logger.js";
 
 export const getServerLogger = serverLogger;
 export const getClientLogger = clientLogger;
+export const getCommonLogger = commonLogger;
