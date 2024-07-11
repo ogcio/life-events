697387039e21cc216728878bfd19ae34656514b7
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-07-11 14:14:51 +0200

    updated requested permissions

--- apps/messaging/app/utils/logto-config.ts
+++ apps/messaging/app/utils/logto-config.ts
@@ -10,12 +10,17 @@ const baseUrl = process.env.NEXT_PUBLIC_MESSAGING_SERVICE_ENTRY_POINT as string;
 const appId = process.env.LOGTO_MESSAGING_APP_ID as string;
 const appSecret = process.env.LOGTO_MESSAGING_APP_SECRET as string;
 const organizationId = "ogcio";
-const citizenScopes = ["messaging:message.self:read"];
+const citizenScopes = [
+  "messaging:message.self:read",
+  "messaging:citizen.self:read",
+  "messaging:citizen.self:write",
+];
 const publicServantScopes = [
   "messaging:message:*",
   "messaging:provider:*",
   "messaging:template:*",
   "messaging:citizen:*",
+  "messaging:event:read",
 ];
 const publicServantExpectedRole = "ogcio:Messaging Public Servant";
 
