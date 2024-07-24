386542a789caf0d4773dbc9cf50d59b351aea955
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-07-24 18:15:34 +0200

    feat(shared-errors): added parent error everywhere

--- packages/shared-errors/errors/third-party-error.ts
+++ packages/shared-errors/errors/third-party-error.ts
@@ -2,8 +2,8 @@ import { LifeEventsError } from "./life-events-error.js";
 
 export class ThirdPartyError extends LifeEventsError {
   readonly errorCode: number = 502;
-  constructor(errorProcess: string, message: string) {
-    super(errorProcess, message);
+  constructor(errorProcess: string, message: string, parentError?: unknown) {
+    super(errorProcess, message, parentError);
     this.name = "THIRD_PARTY_ERROR";
   }
 }
