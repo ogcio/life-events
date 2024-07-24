b6f20a9b0de3cc5de0b7abe46245f3521abdcb05
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-07-24 09:12:27 +0200

    added db check

--- apps/messaging-api/routes/healthcheck.ts
+++ apps/messaging-api/routes/healthcheck.ts
@@ -1,6 +1,20 @@
 import { FastifyInstance } from "fastify";
+import { ServerError } from "shared-errors";
 export default async function healthCheck(app: FastifyInstance) {
   app.get("/health", async () => {
+    await checkDb(app);
+
     return { status: "ok" };
   });
 }
+
+const checkDb = async (app: FastifyInstance): Promise<void> => {
+  const pool = await app.pg.connect();
+  const res = await pool.query('SELECT 1 as "column"');
+  if (res.rowCount !== 1) {
+    throw new ServerError(
+      "HEALTHCHECK",
+      `Expected 1 record, got ${res.rowCount}`,
+    );
+  }
+};
