3f060783d612fd2378a65907841a222af1e90c73
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-07-24 09:23:22 +0200

    normalized error mesage

--- apps/messaging-api/routes/healthcheck.ts
+++ apps/messaging-api/routes/healthcheck.ts
@@ -1,5 +1,9 @@
 import { FastifyInstance } from "fastify";
-import { ServerError } from "shared-errors";
+import { isLifeEventsError, ServerError } from "shared-errors";
+import { getErrorMessage } from "../utils/error-utils";
+
+const ERROR_PROCESS = "HEALTHCHECK";
+
 export default async function healthCheck(app: FastifyInstance) {
   app.get("/health", async () => {
     await checkDb(app);
@@ -9,12 +13,19 @@ export default async function healthCheck(app: FastifyInstance) {
 }
 
 const checkDb = async (app: FastifyInstance): Promise<void> => {
-  const pool = await app.pg.connect();
-  const res = await pool.query('SELECT 1 as "column"');
-  if (res.rowCount !== 1) {
-    throw new ServerError(
-      "HEALTHCHECK",
-      `Expected 1 record, got ${res.rowCount}`,
-    );
+  try {
+    const pool = await app.pg.connect();
+    const res = await pool.query('SELECT 1 as "column"');
+    if (res.rowCount !== 1) {
+      throw new ServerError(
+        ERROR_PROCESS,
+        `Expected 1 record, got ${res.rowCount}`,
+      );
+    }
+  } catch (e) {
+    if (isLifeEventsError(e)) {
+      throw e;
+    }
+    throw new ServerError(ERROR_PROCESS, getErrorMessage(e));
   }
 };
