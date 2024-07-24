5d087001ec8da242063b3c4a695801ac3f655239
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-07-24 18:19:00 +0200

    added the get message method

--- packages/shared-errors/helpers/shared-consts.ts
+++ packages/shared-errors/helpers/shared-consts.ts
@@ -1,3 +1,6 @@
+import { isLifeEventsError } from "shared-errors";
+import { isNativeError } from "util/types";
+
 export enum HttpErrorClasses {
   ServerError = "SERVER_ERROR",
   ValidationError = "VALIDATION_ERROR",
@@ -38,3 +41,33 @@ export const parseHttpErrorClass = (
 };
 
 export const REQUEST_ID_HEADER = "x-life-events-request-id";
+
+export const getErrorMessage = (e: unknown): string => {
+  if (isNativeError(e) || isLifeEventsError(e) || isFastifyError(e)) {
+    return e.message;
+  }
+  switch (typeof e) {
+    case "string":
+      return e;
+    case "bigint":
+    case "number":
+    case "boolean":
+      return String(e);
+    case "object":
+      if (e && "message" in e && typeof e.message === "string") {
+        return e.message;
+      }
+      return e ? e.toString() : "";
+    default:
+      return "";
+  }
+};
+
+const isFastifyError = (
+  e: unknown,
+): e is { code: string; name: string; statusCode?: number; message: string } =>
+  typeof e === "object" &&
+  e !== null &&
+  "code" in e &&
+  "name" in e &&
+  "message" in e;
