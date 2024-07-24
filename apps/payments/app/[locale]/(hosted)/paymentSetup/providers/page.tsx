56d9845836454058c3719894e4a0cbea8b89a087
Sara Zanellato <47299026+peschina@users.noreply.github.com>
2024-07-24 10:41:11 +0200

    [Payments] Organization selector in homepage only (#725)
    * chore: render paymentsmenu component per route
    
    * chore: fix after merge
    
    * chore: remove import
    
    * chore: add missing translation
    
    * chore: get locale from params

--- apps/payments/app/[locale]/(hosted)/paymentSetup/providers/page.tsx
+++ apps/payments/app/[locale]/(hosted)/paymentSetup/providers/page.tsx
@@ -2,35 +2,45 @@ import Link from "next/link";
 import { useTranslations } from "next-intl";
 import ProvidersList from "./ProvidersList";
 import styles from "./Providers.module.scss";
+import PaymentsMenu from "../PaymentsMenu";
 
-export default () => {
+export default ({ params: { locale } }) => {
   const t = useTranslations("PaymentSetup.Providers");
 
   return (
-    <div className="table-container">
-      <section
-        style={{
-          margin: "1rem 0",
-          flex: 1,
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        <div className={styles.headerWrapper}>
-          <h1 className="govie-heading-l">{t("title")}</h1>
-          <Link href="providers/add">
-            <button
-              id="button"
-              data-module="govie-button"
-              className="govie-button"
-            >
-              {t("add")}
-            </button>
-          </Link>
-        </div>
-        <p className="govie-body">{t("description")}</p>
-        <ProvidersList />
-      </section>
+    <div
+      style={{
+        display: "flex",
+        marginTop: "1.3rem",
+        gap: "2rem",
+      }}
+    >
+      <PaymentsMenu locale={locale} />
+      <div className="table-container">
+        <section
+          style={{
+            margin: "1rem 0",
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <div className={styles.headerWrapper}>
+            <h1 className="govie-heading-l">{t("title")}</h1>
+            <Link href="providers/add">
+              <button
+                id="button"
+                data-module="govie-button"
+                className="govie-button"
+              >
+                {t("add")}
+              </button>
+            </Link>
+          </div>
+          <p className="govie-body">{t("description")}</p>
+          <ProvidersList />
+        </section>
+      </div>
     </div>
   );
 };
