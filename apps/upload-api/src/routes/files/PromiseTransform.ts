adf34e7005ad68567290176f3eb8a9e72da43966
Francesco Maida <francesco.maida@nearform.com>
2024-07-25 13:42:37 +0200

    Update PromiseTransform.ts

--- apps/upload-api/src/routes/files/PromiseTransform.ts
+++ apps/upload-api/src/routes/files/PromiseTransform.ts
@@ -2,7 +2,7 @@ import { Transform } from "stream";
 /**
  * This class creates a transform that flushes the streamed data only
  * after the passed promise is resolved.
- * If the promise rejects an error is thrown and stream destroyed and
+ * If the promise rejects an error is thrown, the stream is destroyed and
  * the last chunk of data is not passed downstream.
  * it is used to prevent streaming the very last chunk of data downstream if the
  * antivirus promise rejects detecting infected data stream.
