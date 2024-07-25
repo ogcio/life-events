b7628cfa8465ca67663710f59dd550d2cdaabce9
SamSalvatico <40636569+SamSalvatico@users.noreply.github.com>
2024-07-25 14:19:41 +0200

    Feature(messaging): prepare language selector (#716)
    * feat(messaging-api): added getProfileClient to auth factory
    
    * moved header to use same compoentns as other services
    
    * moved header to use same compoentns as other services
    
    * Feature(messaging): 15380 - preferred language (#717)
    
    * feat(messaging): start adding language persistence
    
    * updated env vars
    
    * removed console log
    
    * removed useless
    
    * delete next locale cookie
    
    * set lang in middleware
    
    * added EOF
    
    * added EOF
    
    * Feature(profile): 15380 - integrate logto into profile (#720)
    
    * added authentication factory
    
    * removed x-user-id hader management
    
    * added logto envs
    
    * added permissions
    
    * udpated user profile usages
    
    * fixed casing in dockerfile
    
    * added argument
    
    * fixed dockerfile
    
    * Added EOF
    
    * Added EOF
    
    * added cookie secret
    
    * added logto endpoint
    
    * Added callback route
    
    * Added login and signout routes
    
    * updated app id
    
    * updated logout path
    
    * added api auth to nodemon
    
    * Added EOF
    
    * Chore(messaging): 15380 - use profile sdk with token (#723)
    
    * use profile with auth token
    
    * fixed docker casing
    
    * fixed env assignment in docker

--- apps/profile/app/utils/authentication-factory.ts
+++ apps/profile/app/utils/authentication-factory.ts
@@ -1,26 +1,10 @@
 import { BaseAuthenticationContext } from "auth/base-authentication-context";
 import { getAuthenticationContextConfig } from "./logto-config";
-import { Messaging, Profile } from "building-blocks-sdk";
+import { Profile } from "building-blocks-sdk";
 
 export class AuthenticationFactory {
   static getInstance(): BaseAuthenticationContext {
     return new BaseAuthenticationContext(getAuthenticationContextConfig());
-  }
-
-  static async getMessagingClient(params?: {
-    token?: string;
-    authenticationContext?: BaseAuthenticationContext;
-  }): Promise<Messaging> {
-    if (params?.token) {
-      return new Messaging(params.token);
-    }
-    if (params?.authenticationContext) {
-      return new Messaging(await params.authenticationContext.getAccessToken());
-    }
-
-    const token = await this.getInstance().getAccessToken();
-
-    return new Messaging(token);
   }
 
   static async getProfileClient(params?: {
