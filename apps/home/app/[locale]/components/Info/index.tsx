4085fdbd901d1811393a0ddf6561f720d6fd9b67
Norbert Nagy <norbert.nagy@nearform.com>
2024-07-26 15:49:27 +0300

    update

--- apps/home/app/[locale]/components/Info/index.tsx
+++ apps/home/app/[locale]/components/Info/index.tsx
@@ -1,6 +1,6 @@
 import "design-system/dist/style.css";
 import "design-system/dist/esm/index.css";
-import "./Info.module.scss";
+import "./Info.scss";
 import { getTranslations } from "next-intl/server";
 import Image from "next/image";
 import hero from "../../../../public/landingPage/hero.png";
