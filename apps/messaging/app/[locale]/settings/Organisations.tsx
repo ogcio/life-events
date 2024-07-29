f575b59784bc19e243e9d6a049f8d50a19f2db8f
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-07-29 14:14:00 +0200

    Organisation setting id

--- apps/messaging/app/[locale]/settings/Organisations.tsx
+++ apps/messaging/app/[locale]/settings/Organisations.tsx
@@ -29,7 +29,10 @@ export default async () => {
       </thead>
       <tbody className="govie-table__body">
         {data?.map((organisationSetting) => (
-          <tr className="govie-table__row" key={organisationSetting.id}>
+          <tr
+            className="govie-table__row"
+            key={organisationSetting.organisationSettingId}
+          >
             <th className="govie-table__header govie-table__header--vertical-centralized govie-body-s">
               {/* {invitation.organisationId} At the moment we want to show "Life Events" as fixed value*/}
               Life Events
@@ -46,7 +49,7 @@ export default async () => {
                   className="govie-link govie-!-margin-right-3"
                   href={(() => {
                     const url = new URL(
-                      `${userOrganisationsRoutes.url}/${organisationSetting.id}`,
+                      `${userOrganisationsRoutes.url}/${organisationSetting.organisationSettingId}`,
                       process.env.HOST_URL,
                     );
                     return url.href;
