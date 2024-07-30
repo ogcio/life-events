7d1fd6c0c51f7987cae2daaa4a62717fa57bbe98
Norbert Nagy <norbert.nagy@nearform.com>
2024-07-30 11:41:01 +0300

    Update

--- apps/payments/app/[locale]/(hosted)/paymentSetup/PaymentsMenu.tsx
+++ apps/payments/app/[locale]/(hosted)/paymentSetup/PaymentsMenu.tsx
@@ -12,53 +12,14 @@ const Icon = ds.Icon;
 
 type Props = {
   locale: string;
-  organizations?: OrganizationData[];
-  defaultOrganization?: string;
-  disableOrgSelector?: boolean;
 };
 
-// load component asynchronously, only when it is needed
-const OrganizationSelector = dynamic(
-  () => import("shared-components").then((mod) => mod.OrganizationSelector),
-  {
-    ssr: true,
-  },
-);
-
-export default ({
-  locale,
-  organizations,
-  defaultOrganization,
-  disableOrgSelector,
-}: Props) => {
+export default ({ locale }: Props) => {
   const t = useTranslations("Menu");
-
-  async function handleSubmit(formData: FormData) {
-    "use server";
-    const context = AuthenticationFactory.getInstance();
-    const selectedOrganization = formData.get("organization") as string;
-    context.setSelectedOrganization(selectedOrganization);
-    redirect("/", RedirectType.replace);
-  }
 
   return (
     <>
       <div>
-        <Suspense fallback={<h3>{t("loading")}</h3>}>
-          {organizations && organizations.length > 1 && (
-            <OrganizationSelector
-              title="Department"
-              actionTitle="Change department"
-              organizations={organizations.map((org) => ({
-                name: org.name,
-                id: org.id,
-              }))}
-              defaultOrganization={defaultOrganization}
-              handleSubmit={handleSubmit}
-              disabled={disableOrgSelector}
-            ></OrganizationSelector>
-          )}
-        </Suspense>
         <div>
           <ol className={`govie-list govie-list--spaced ${styles.container}`}>
             <li tabIndex={0}>
