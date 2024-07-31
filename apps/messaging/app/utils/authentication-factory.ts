0c09d4037d288c8167ec6e4772143e7891c7c7c6
Sara Zanellato <sara.zanellato@nearform.com>
2024-07-31 14:42:58 +0200

    fix: get token from route handler in messaging

--- apps/messaging/app/utils/authentication-factory.ts
+++ apps/messaging/app/utils/authentication-factory.ts
@@ -1,41 +1,34 @@
 import { BaseAuthenticationContext } from "auth/base-authentication-context";
 import { getAuthenticationContextConfig } from "./logto-config";
 import { Messaging, Profile } from "building-blocks-sdk";
+import { headers } from "next/headers";
 
 export class AuthenticationFactory {
   static getInstance(): BaseAuthenticationContext {
     return new BaseAuthenticationContext(getAuthenticationContextConfig());
   }
 
-  static async getMessagingClient(params?: {
-    token?: string;
-    authenticationContext?: BaseAuthenticationContext;
-  }): Promise<Messaging> {
-    if (params?.token) {
-      return new Messaging(params.token);
-    }
-    if (params?.authenticationContext) {
-      return new Messaging(await params.authenticationContext.getAccessToken());
-    }
+  static async getToken(): Promise<string> {
+    const cookieHeader = headers().get("cookie") as string;
 
-    const token = await this.getInstance().getAccessToken();
+    const res = await fetch(
+      new URL(
+        "/api/token",
+        process.env.NEXT_PUBLIC_MESSAGING_SERVICE_ENTRY_POINT as string,
+      ),
+      { headers: { cookie: cookieHeader } },
+    );
+    const { token } = await res.json();
+    return token;
+  }
 
+  static async getMessagingClient(): Promise<Messaging> {
+    const token = await this.getToken();
     return new Messaging(token);
   }
 
-  static async getProfileClient(params?: {
-    token?: string;
-    authenticationContext?: BaseAuthenticationContext;
-  }): Promise<Profile> {
-    if (params?.token) {
-      return new Profile(params.token);
-    }
-    if (params?.authenticationContext) {
-      return new Profile(await params.authenticationContext.getAccessToken());
-    }
-
-    const token = await this.getInstance().getAccessToken();
-
+  static async getProfileClient(): Promise<Profile> {
+    const token = await this.getToken();
     return new Profile(token);
   }
 }
