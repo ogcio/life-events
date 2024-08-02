eb6c77dfc2ab958f8fdecafe48bc77befab8cd92
Francesco Maida <francesco.maida@nearform.com>
2024-08-02 09:27:03 +0200

    new auth changes

--- apps/upload/app/utils/authentication-factory.ts
+++ apps/upload/app/utils/authentication-factory.ts
@@ -1,24 +1,27 @@
 import { BaseAuthenticationContext } from "auth/base-authentication-context";
 import { getAuthenticationContextConfig } from "./logto-config";
 import { Upload } from "building-blocks-sdk";
+import { headers } from "next/headers";
 
 export class AuthenticationFactory {
   static getInstance(): BaseAuthenticationContext {
     return new BaseAuthenticationContext(getAuthenticationContextConfig());
   }
 
-  static async getUploadClient(params?: {
-    token?: string;
-    authenticationContext?: BaseAuthenticationContext;
-  }): Promise<Upload> {
-    if (params?.token) {
-      return new Upload(params.token);
-    }
-    if (params?.authenticationContext) {
-      return new Upload(await params.authenticationContext.getAccessToken());
-    }
+  static async getToken(): Promise<string> {
+    // call a route handler that retrieves the cached token
+    // we need to forward the cookie header or the request won't be authenticated
+    const cookieHeader = headers().get("cookie") as string;
 
-    const token = await this.getInstance().getAccessToken();
+    const res = await fetch(new URL("/api/token", process.env.HOST_URL), {
+      headers: { cookie: cookieHeader },
+    });
+    const { token } = await res.json();
+    return token;
+  }
+
+  static async getUploadClient(): Promise<Upload> {
+    const token = await this.getToken();
     return new Upload(token);
   }
 }
