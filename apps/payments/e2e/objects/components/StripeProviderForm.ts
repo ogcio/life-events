596c5298b49c7c3e803bef0621bf65186340c4c2
Sara Zanellato <sara.zanellato@nearform.com>
2024-08-08 10:01:50 +0200

    chore: test stripe provider editing

--- apps/payments/e2e/objects/components/StripeProviderForm.ts
+++ apps/payments/e2e/objects/components/StripeProviderForm.ts
@@ -34,6 +34,18 @@ export class StripeProviderForm {
     await this.secretKey.fill(key);
   }
 
+  async checkName(name: string) {
+    await expect(this.nameInput).toHaveValue(name);
+  }
+
+  async checkPublishableKey(key: string) {
+    await expect(this.publishableKey).toHaveValue(key);
+  }
+
+  async checkSecretKey(key: string) {
+    await expect(this.secretKey).toHaveValue(key);
+  }
+
   async expectValidationError(expectedError: StripeValidationError) {
     const errorMessage = await this.page.getByText(
       providerValidationErrorTexts[expectedError],
--- apps/payments/e2e/specs/providers/stripe.spec.ts
+++ apps/payments/e2e/specs/providers/stripe.spec.ts
@@ -1,5 +1,6 @@
 import { Page } from "@playwright/test";
 import { test } from "../../fixtures/test";
+import { test as testWithProvider } from "../../fixtures/providersFixtures";
 import {
   mockStripePublishableKey,
   mockStripeSecretKey,
@@ -14,8 +15,9 @@ import {
   description,
 } from "allure-js-commons";
 import { AddStripeProviderPage } from "../../objects/providers/AddStripeProviderPage";
+import { EditStripeProviderPage } from "../../objects/providers/EditStripeProviderPage";
 
-test.describe("Stripe provider", () => {
+test.describe("Stripe provider creation", () => {
   let page: Page;
   let providerName: string;
 
@@ -159,4 +161,190 @@ test.describe("Stripe provider", () => {
     await providersPage.goto();
     await providersPage.checkProviderNotVisible(providerName);
   });
+});
+
+testWithProvider.describe("Stripe provider editing", () => {
+  let page: Page;
+
+  testWithProvider.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+  });
+
+  testWithProvider(
+    "should edit a Stripe provider @regression @normal",
+    async ({ stripeProvider }) => {
+      await description(
+        "This test checks the successful editing of a Stripe provider.",
+      );
+      await owner("OGCIO");
+      await tags("Providers", "Stripe");
+      await severity(Severity.NORMAL);
+
+      const newSecretKey = "new_sk_test_123456";
+      const newPublishableKey = "new_pk_test_123456";
+      const newProviderName = `${stripeProvider} edited`;
+
+      await page.goto(paymentSetupUrl);
+
+      const providersMenuLink = await page.getByRole("link", {
+        name: "Providers",
+      });
+      await providersMenuLink.click();
+
+      const providersPage = new ProvidersPage(page);
+      await providersPage.editProvider(stripeProvider);
+      const editProviderPage = new EditStripeProviderPage(page);
+      await editProviderPage.checkHeaderVisible();
+      await editProviderPage.providerForm.checkName(stripeProvider);
+      await editProviderPage.providerForm.enterName(newProviderName);
+      await editProviderPage.providerForm.checkPublishableKey(
+        mockStripePublishableKey,
+      );
+      await editProviderPage.providerForm.enterPublishableKey(
+        newPublishableKey,
+      );
+      await editProviderPage.providerForm.checkSecretKey(mockStripeSecretKey);
+      await editProviderPage.providerForm.enterSecretKey(newSecretKey);
+      await editProviderPage.saveChanges();
+
+      await providersPage.checkProviderVisible(newProviderName);
+      await providersPage.editProvider(newProviderName);
+      await editProviderPage.providerForm.checkName(newProviderName);
+      await editProviderPage.providerForm.checkPublishableKey(
+        newPublishableKey,
+      );
+      await editProviderPage.providerForm.checkSecretKey(newSecretKey);
+    },
+  );
+
+  testWithProvider(
+    "should disable and enable a stripe provider @regression @normal",
+    async ({ stripeProvider }) => {
+      await description(
+        "This test checks that a stripe provider is successfully disabled and enabled.",
+      );
+      await owner("OGCIO");
+      await tags("Providers", "Stripe");
+      await severity(Severity.NORMAL);
+
+      await page.goto(paymentSetupUrl);
+
+      const providersMenuLink = await page.getByRole("link", {
+        name: "Providers",
+      });
+      await providersMenuLink.click();
+
+      const providersPage = new ProvidersPage(page);
+      await providersPage.editProvider(stripeProvider);
+      const editProviderPage = new EditStripeProviderPage(page);
+      await editProviderPage.checkHeaderVisible();
+      await editProviderPage.disableProvider();
+      await providersPage.checkProviderIsDisabled(stripeProvider);
+
+      await providersPage.editProvider(stripeProvider);
+      await editProviderPage.checkHeaderVisible();
+      await editProviderPage.enableProvider();
+      await providersPage.checkProviderIsEnabled(stripeProvider);
+    },
+  );
+
+  testWithProvider(
+    "should not edit a Stripe provider if name is missing @regression @normal",
+    async ({ stripeProvider }) => {
+      await description(
+        "This test checks that while editing a Stripe provider it cannot be saved if name is missing.",
+      );
+      await owner("OGCIO");
+      await tags("Providers", "Stripe");
+      await severity(Severity.NORMAL);
+
+      await page.goto(paymentSetupUrl);
+
+      const providersMenuLink = await page.getByRole("link", {
+        name: "Providers",
+      });
+      await providersMenuLink.click();
+
+      const providersPage = new ProvidersPage(page);
+      await providersPage.editProvider(stripeProvider);
+      const editProviderPage = new EditStripeProviderPage(page);
+      await editProviderPage.checkHeaderVisible();
+      await editProviderPage.providerForm.checkName(stripeProvider);
+      await editProviderPage.providerForm.enterName("");
+      await editProviderPage.saveChanges();
+      await editProviderPage.providerForm.expectValidationError("nameRequired");
+    },
+  );
+
+  testWithProvider(
+    "should not edit a stripe provider if publishable key is missing @regression @normal",
+    async ({ stripeProvider }) => {
+      await description(
+        "This test checks that while editing a stripe provider it cannot be saved if publishable key is missing.",
+      );
+      await owner("OGCIO");
+      await tags("Providers", "Stripe");
+      await severity(Severity.NORMAL);
+
+      await page.goto(paymentSetupUrl);
+
+      const providersMenuLink = await page.getByRole("link", {
+        name: "Providers",
+      });
+      await providersMenuLink.click();
+
+      const providersPage = new ProvidersPage(page);
+      await providersPage.editProvider(stripeProvider);
+      const editProviderPage = new EditStripeProviderPage(page);
+      await editProviderPage.checkHeaderVisible();
+      await editProviderPage.providerForm.checkPublishableKey(
+        mockStripePublishableKey,
+      );
+      await editProviderPage.providerForm.enterPublishableKey("");
+      await editProviderPage.saveChanges();
+      await editProviderPage.providerForm.expectValidationError(
+        "publishableKeyRequired",
+      );
+
+      await providersPage.goto();
+      await providersPage.editProvider(stripeProvider);
+      await editProviderPage.providerForm.checkPublishableKey(
+        mockStripePublishableKey,
+      );
+    },
+  );
+
+  testWithProvider(
+    "should not edit a stripe provider if secret key is missing @regression @normal",
+    async ({ stripeProvider }) => {
+      await description(
+        "This test checks that while editing a stripe provider it cannot be saved if secret key is missing.",
+      );
+      await owner("OGCIO");
+      await tags("Providers", "Stripe");
+      await severity(Severity.NORMAL);
+
+      await page.goto(paymentSetupUrl);
+
+      const providersMenuLink = await page.getByRole("link", {
+        name: "Providers",
+      });
+      await providersMenuLink.click();
+
+      const providersPage = new ProvidersPage(page);
+      await providersPage.editProvider(stripeProvider);
+      const editProviderPage = new EditStripeProviderPage(page);
+      await editProviderPage.checkHeaderVisible();
+      await editProviderPage.providerForm.checkSecretKey(mockStripeSecretKey);
+      await editProviderPage.providerForm.enterSecretKey("");
+      await editProviderPage.saveChanges();
+      await editProviderPage.providerForm.expectValidationError(
+        "secretKeyRequired",
+      );
+
+      await providersPage.goto();
+      await providersPage.editProvider(stripeProvider);
+      await editProviderPage.providerForm.checkSecretKey(mockStripeSecretKey);
+    },
+  );
 });
