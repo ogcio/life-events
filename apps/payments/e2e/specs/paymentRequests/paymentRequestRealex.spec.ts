abdbb403782c572305df4c67f153d2c2d70c2fd2
Sara Zanellato <47299026+peschina@users.noreply.github.com>
2024-08-12 10:47:01 +0200

    [Payments] E2E test - Active and inactive payment request creation with Realex provider  (#821)
    * chore: test inactive payment request with bank transfer provider
    
    * chore: delete old pages
    
    * fix: fix tests
    
    * chore: move file
    
    * chore: test active payment request
    
    * chore: test payment request creation with open banking provider
    
    * chore: test payment request creation with stripe
    
    * chore: add realex tests

---
+++ apps/payments/e2e/specs/paymentRequests/paymentRequestRealex.spec.ts
@@ -0,0 +1,144 @@
+import { Page } from "@playwright/test";
+import { test } from "../../fixtures/providersFixtures";
+import {
+  Severity,
+  owner,
+  tags,
+  severity,
+  description,
+} from "allure-js-commons";
+import { PaymentRequestsPage } from "../../objects/paymentRequests/PaymentRequestsListPage";
+import { PaymentRequestFormPage } from "../../objects/paymentRequests/PaymentRequestFormPage";
+import {
+  mockAmount,
+  mockPaymentRequestReference,
+  mockRedirectUrl,
+  paymentRequestDescription,
+} from "../../utils/mocks";
+import { PaymentRequestDetailsPage } from "../../objects/paymentRequests/PaymentRequestDetailsPage";
+import { InactivePayPage } from "../../objects/payments/InactivePayPage";
+import { PreviewPayPage } from "../../objects/payments/PreviewPayPage";
+
+test.describe("Payment Request with realex provider", () => {
+  let page: Page;
+  let name: string;
+
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+  });
+
+  test.beforeEach(async () => {
+    name = `Test ${Date.now()}`;
+  });
+
+  test("should create an inactive payment request with a realex provider @regression @normal", async ({
+    realexProvider,
+    context,
+  }) => {
+    await description(
+      "This test checks the successful creation of an inactive payment request with a realex provider.",
+    );
+    await owner("OGCIO");
+    await tags("Payment Request", "Realex");
+    await severity(Severity.NORMAL);
+
+    const paymentRequestsPage = new PaymentRequestsPage(page);
+    await paymentRequestsPage.goto();
+    await paymentRequestsPage.gotoCreate();
+
+    const createPaymentRequestPage = new PaymentRequestFormPage(page);
+    await createPaymentRequestPage.enterTitle(name);
+    await createPaymentRequestPage.enterDescription(paymentRequestDescription);
+    await createPaymentRequestPage.selectCardAccount(realexProvider);
+    await createPaymentRequestPage.enterReference(mockPaymentRequestReference);
+    await createPaymentRequestPage.enterAmount(mockAmount);
+    await createPaymentRequestPage.selectAllowAmountOverride();
+    await createPaymentRequestPage.selectCustomAmount();
+    await createPaymentRequestPage.enterRedirectURL(mockRedirectUrl);
+    await createPaymentRequestPage.selectInactiveStatus();
+    await createPaymentRequestPage.saveChanges();
+
+    const detailsPage = new PaymentRequestDetailsPage(page);
+    await detailsPage.checkHeader();
+    await detailsPage.checkTitle(name);
+    await detailsPage.checkDescription(paymentRequestDescription);
+    await detailsPage.checkStatus("inactive");
+    await detailsPage.checkAccounts([{ name: realexProvider, type: "realex" }]);
+    await detailsPage.checkAmount(mockAmount);
+    await detailsPage.checkRedirectUrl(mockRedirectUrl);
+    await detailsPage.checkAmountOverrideOption(true);
+    await detailsPage.checkCustomAmountOption(true);
+    await detailsPage.checkEmptyPaymentsList();
+
+    const link = await detailsPage.getPaymentLink();
+    const newPage = await context.newPage();
+    await newPage.goto(link);
+    const inactivePayPage = new InactivePayPage(newPage);
+    await inactivePayPage.checkHeader();
+    await inactivePayPage.checkDescription();
+
+    await paymentRequestsPage.goto();
+    await paymentRequestsPage.checkHeader();
+    await paymentRequestsPage.checkRequestIsVisible(name);
+    await paymentRequestsPage.checkBeneficiaryAccounts(name, [realexProvider]);
+    await paymentRequestsPage.checkAmount(name, mockAmount);
+    await paymentRequestsPage.checkStatus(name, "inactive");
+    await paymentRequestsPage.checkReference(name, mockPaymentRequestReference);
+  });
+
+  test("should create an active payment request with a realex provider @smoke @critical", async ({
+    realexProvider,
+    context,
+  }) => {
+    await description(
+      "This test checks the successful creation of an inactive payment request with a realex provider.",
+    );
+    await owner("OGCIO");
+    await tags("Payment Request", "Realex");
+    await severity(Severity.CRITICAL);
+
+    const paymentRequestsPage = new PaymentRequestsPage(page);
+    await paymentRequestsPage.goto();
+    await paymentRequestsPage.gotoCreate();
+
+    const createPaymentRequestPage = new PaymentRequestFormPage(page);
+    await createPaymentRequestPage.enterTitle(name);
+    await createPaymentRequestPage.enterDescription(paymentRequestDescription);
+    await createPaymentRequestPage.selectCardAccount(realexProvider);
+    await createPaymentRequestPage.enterReference(mockPaymentRequestReference);
+    await createPaymentRequestPage.enterAmount(mockAmount);
+    await createPaymentRequestPage.enterRedirectURL(mockRedirectUrl);
+    await createPaymentRequestPage.selectActiveStatus();
+    await createPaymentRequestPage.saveChanges();
+
+    const detailsPage = new PaymentRequestDetailsPage(page);
+    await detailsPage.checkHeader();
+    await detailsPage.checkTitle(name);
+    await detailsPage.checkDescription(paymentRequestDescription);
+    await detailsPage.checkStatus("active");
+    await detailsPage.checkAccounts([{ name: realexProvider, type: "realex" }]);
+    await detailsPage.checkAmount(mockAmount);
+    await detailsPage.checkRedirectUrl(mockRedirectUrl);
+    await detailsPage.checkAmountOverrideOption(false);
+    await detailsPage.checkCustomAmountOption(false);
+    await detailsPage.checkEmptyPaymentsList();
+
+    const link = await detailsPage.getPaymentLink();
+    const newPage = await context.newPage();
+    await newPage.goto(link);
+    const previewPayPage = new PreviewPayPage(newPage);
+    await previewPayPage.checkHeader();
+    await previewPayPage.checkAmount(mockAmount);
+    await previewPayPage.checkPaymentMethodHeader();
+    await previewPayPage.checkPaymentMethodVisible("card");
+    await previewPayPage.checkButton();
+
+    await paymentRequestsPage.goto();
+    await paymentRequestsPage.checkHeader();
+    await paymentRequestsPage.checkRequestIsVisible(name);
+    await paymentRequestsPage.checkBeneficiaryAccounts(name, [realexProvider]);
+    await paymentRequestsPage.checkAmount(name, mockAmount);
+    await paymentRequestsPage.checkStatus(name, "active");
+    await paymentRequestsPage.checkReference(name, mockPaymentRequestReference);
+  });
+});
