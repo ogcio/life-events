a2c6e400268704c8899b82617443960e9c9ba352
SamSalvatico <40636569+SamSalvatico@users.noreply.github.com>
2024-08-19 11:21:09 +0200

    Test(messaging-api): adding bruno tests (#833)
    * started adding bruno tests
    
    * removed duplicated start app log
    
    * added user import tests
    
    * added 404 requests
    
    * added templates tests
    
    * Added users tests
    
    * fixed active only
    
    * added users requests
    
    * Added non-active apis
    
    * updated values
    
    * removed unused organisation id query string
    
    * added get users for import
    
    * Added basic token
    
    * Update apps/messaging-api/routes/users/index.ts
    
    Co-authored-by: Norbert Nagy <nagy.norbert1989@gmail.com>
    
    * fixed
    
    * updated check in tests
    
    ---------
    
    Co-authored-by: Norbert Nagy <nagy.norbert1989@gmail.com>

--- apps/messaging-api/index.ts
+++ apps/messaging-api/index.ts
@@ -6,12 +6,11 @@ import { getLoggingConfiguration } from "logging-wrapper";
 
 const app = await build(getLoggingConfiguration());
 
-app.listen({ host: "0.0.0.0", port: 8002 }, (err, address) => {
+app.listen({ host: "0.0.0.0", port: 8002 }, (err) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
-  app.log.info(`app listening at ${address}`);
 });
 
 await app.ready();
