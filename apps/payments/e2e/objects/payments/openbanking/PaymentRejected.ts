f8428f9418de36cc740edf237bba295948762f88
Sara Zanellato <47299026+peschina@users.noreply.github.com>
2024-08-23 14:59:21 +0200

    chore: handle tl transaction status (#861)

--- apps/payments/e2e/objects/payments/openbanking/PaymentRejected.ts
+++ apps/payments/e2e/objects/payments/openbanking/PaymentRejected.ts
@@ -3,6 +3,7 @@ import { expect, Locator, Page } from "@playwright/test";
 export class PaymentRejected {
   private readonly header: Locator;
   private readonly infoText: Locator;
+  private readonly loadingText: Locator;
   private readonly goBackBtn: Locator;
 
   constructor(public readonly page: Page) {
@@ -12,12 +13,13 @@ export class PaymentRejected {
     this.infoText = this.page.getByText(
       "Mock Ireland Payments – Redirect Flow rejected your transaction. Go back to payments to change bank or try again.",
     );
+    this.loadingText = this.page.getByText("Confirming your payment");
     this.goBackBtn = this.page.getByRole("button", { name: "Go back" });
   }
 
   async checkIsFailed() {
+    await expect(this.loadingText.or(this.header)).toBeVisible();
     await expect(this.infoText).toBeVisible();
-    await expect(this.header).toBeVisible();
   }
 
   async goBack() {
