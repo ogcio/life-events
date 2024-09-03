79f770ab7f5ecc0abd7ff4636fac50e33d58cbfd
Francesco Maida <francesco.maida@nearform.com>
2024-09-03 17:44:24 +0200

    chore(explorer) initial batch of e2e for explorer app

---
+++ apps/upload/e2e/utils/constants.ts
@@ -0,0 +1,11 @@
+export const landingPageUrl = "http://localhost:3008";
+
+export const password = "123";
+export const citizens = ["Peter Parker", "Bruce Wayne"];
+export const publicServants = ["Tony Stark", "John Doe"];
+export const myGovIdMockSettings = {
+  publicServantEmailDomain: "gov.ie",
+  citizenEmailDomain: "mail.ie",
+};
+
+export const SAFE_PID_FILENAME = "ngrok.pid";
---
+++ apps/upload/e2e/utils/mocks.ts
@@ -0,0 +1,31 @@
+export const mockIban = "IE29AIBK93115212345678";
+export const otherMockIban = "IE64IRCE92050112345678";
+
+export const mockAccountHolderName = "Name Surname";
+
+export const mockStripeSecretKey = "sk_test_123456";
+export const mockStripePublishableKey = "pk_test_123456";
+
+export const mockRealexMerchantId = "mock_realex_id";
+export const mockRealexSharedSecret = "mock_realex_secret";
+
+export const paymentRequestDescription = "A test payment request";
+export const mockPaymentRequestReference = "A reference";
+export const mockRedirectUrl = "https://www.google.com";
+export const mockAmount = "100.00";
+
+export const mockPhoneNumber = "0598638138";
+export const mockAddress = {
+  street: "Lotamore",
+  country: "Ireland",
+  state: "County Cork",
+  city: "GlanMire",
+  ZIP: "T23",
+};
+
+export const realexCardData = {
+  successNumber: "4263970000005262",
+  failNumber: "4000120000001154",
+  expiry: "0230",
+  code: "123",
+};
