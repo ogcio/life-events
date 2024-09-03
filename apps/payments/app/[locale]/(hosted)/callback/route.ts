fbb15cd98a8905fa21d6d80d08cb06d09ba8f40b
Norbert Nagy <nagy.norbert1989@gmail.com>
2024-09-03 14:53:40 +0300

    20101 e2e inactive public servant (#909)
    * feat(20101): e2e test inactive public servant page
    
    * Update
    
    * update
    
    * Update
    
    * Update

--- apps/payments/app/[locale]/(hosted)/callback/route.ts
+++ apps/payments/app/[locale]/(hosted)/callback/route.ts
@@ -2,14 +2,24 @@ import { handleSignIn } from "@logto/next/server-actions";
 import { redirect } from "next/navigation";
 import { NextRequest } from "next/server";
 import logtoConfig, {
+  logtoUserIdCookieName,
   postLoginRedirectUrlCookieName,
 } from "../../../../libraries/logtoConfig";
 import { cookies } from "next/headers";
+import { AuthenticationFactory } from "../../../../libraries/authentication-factory";
 
 const DEFAULT_POST_LOGIN_REDIRECT_URL = "/";
 
 export async function GET(request: NextRequest) {
   await handleSignIn(logtoConfig, request.nextUrl.searchParams);
+
+  const user = await AuthenticationFactory.getInstance().getUser();
+
+  /**
+   * In some scenarios in E2E tests, user ID is required. Because this information
+   * is not on the page, we must store it in a cookie for later usage.
+   */
+  cookies().set(logtoUserIdCookieName, user.id);
 
   const postRedirectUrl = cookies().get(postLoginRedirectUrlCookieName)?.value;
   if (postRedirectUrl) {
