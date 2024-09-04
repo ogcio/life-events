23c4875d87963aed1635755792b919b43834b27f
Sara Zanellato <sara.zanellato@nearform.com>
2024-09-04 09:30:20 +0200

    chore: test update payment request event

--- apps/payments/e2e/specs/auditLogs/auditLogs.spec.ts
+++ apps/payments/e2e/specs/auditLogs/auditLogs.spec.ts
@@ -10,6 +10,8 @@ import { AuditLogsListPage } from "../../objects/auditLogs/AuditLogsListPage";
 import { AuditLogDetailsPage } from "../../objects/auditLogs/AuditLogDetailsPage";
 import { AuditLogEventType, ORGANISATIONS } from "../../utils/constants";
 import { EditManualBankTransferProviderPage } from "../../objects/providers/EditManualBankTransferProviderPage";
+import { PaymentRequestDetailsPage } from "../../objects/paymentRequests/PaymentRequestDetailsPage";
+import { PaymentRequestFormPage } from "../../objects/paymentRequests/PaymentRequestFormPage";
 
 test.describe("Audit Logs", () => {
   test("should create an audit log event when a new provider is created @regression @normal", async ({
@@ -89,6 +91,51 @@ test.describe("Audit Logs", () => {
     await severity(Severity.NORMAL);
 
     const eventType = AuditLogEventType.PAYMENT_REQUEST_CREATE;
+    const auditLogsPage = new AuditLogsListPage(publicServantPage);
+    await auditLogsPage.goto();
+    await auditLogsPage.checkHeader();
+    await auditLogsPage.checkFilters();
+    await auditLogsPage.checkAuditLog(
+      paymentRequestWithManualBankTransferProvider.id,
+      eventType,
+    );
+    await auditLogsPage.goToDetails(
+      paymentRequestWithManualBankTransferProvider.id,
+      eventType,
+    );
+
+    const detailsPage = new AuditLogDetailsPage(publicServantPage);
+    await detailsPage.checkEventName(eventType);
+    await detailsPage.checkTimestampLabel();
+    await detailsPage.checkEventType(eventType);
+    await detailsPage.checkUserId();
+    await detailsPage.checkOrganizationId(ORGANISATIONS[0].id);
+    await detailsPage.checkMetadata(
+      paymentRequestWithManualBankTransferProvider.id,
+      "payment_request",
+    );
+  });
+
+  test("should create an audit log event when a payment request is updated @regression @normal", async ({
+    paymentRequestWithManualBankTransferProvider,
+    publicServantPage,
+  }) => {
+    await description(
+      "This test checks the successful creation of an audit log when a payment request is updated.",
+    );
+    await owner("OGCIO");
+    await tags("Audit Logs", "Providers");
+    await severity(Severity.NORMAL);
+
+    const prDetailsPage = new PaymentRequestDetailsPage(publicServantPage);
+    await prDetailsPage.gotoEdit();
+    const editPaymentRequestPage = new PaymentRequestFormPage(
+      publicServantPage,
+    );
+    await editPaymentRequestPage.enterAmount("10.00");
+    await editPaymentRequestPage.saveChanges();
+
+    const eventType = AuditLogEventType.PAYMENT_REQUEST_UPDATE;
     const auditLogsPage = new AuditLogsListPage(publicServantPage);
     await auditLogsPage.goto();
     await auditLogsPage.checkHeader();
