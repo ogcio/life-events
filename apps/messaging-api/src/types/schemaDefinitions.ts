850997855e5b11c1af2a6f0235f0f83a7be4dfc7
SamSalvatico <40636569+SamSalvatico@users.noreply.github.com>
2024-09-05 10:57:36 +0200

    Chore(messaging-api): remove sender from messages (#930)
    * chore(messaging-api): removed sender columns migration
    
    * removed fields from definitions
    
    * sdks

--- apps/messaging-api/src/types/schemaDefinitions.ts
+++ apps/messaging-api/src/types/schemaDefinitions.ts
@@ -98,19 +98,6 @@ export const ReadMessageSchema = Type.Object({
     description: "True if the message has already been seen by the recipient",
   }),
   security: SecurityLevelsSchema,
-  senderUserProfileId: Type.Union([
-    Type.String({
-      description:
-        "Unique id of the sender from the Life Events building block",
-    }),
-    Type.Null(),
-  ]),
-  senderApplicationId: Type.Union([
-    Type.String({
-      description: "Unique id of the M2M application that sent the message",
-    }),
-    Type.Null(),
-  ]),
 });
 export type ReadMessage = Static<typeof ReadMessageSchema>;
 
