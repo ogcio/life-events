96adc69793be6ae85cc7337146f06095f3a4dc0c
Sara Zanellato <47299026+peschina@users.noreply.github.com>
2024-09-05 08:55:31 +0200

    [Payments] E2E tests -  public servant transaction view and audit logs for transaction updates (#927)
    * chore: test payment request creation event
    
    * chore: test update payment request event
    
    * chore: test delete payment request event
    
    * chore: test create transaction event
    
    * fix tests
    
    * chore: test transactions view for public servant
    
    * chore: test transaction update event
    
    * chore: check transaction status on citizen page

--- apps/payments/e2e/objects/auditLogs/AuditLogsListPage.tsx
+++ apps/payments/e2e/objects/auditLogs/AuditLogsListPage.tsx
@@ -1,5 +1,6 @@
 import { type Page, type Locator, expect } from "@playwright/test";
 import { AuditLogEventTitles, AuditLogEventType } from "../../utils/constants";
+import { getUserId } from "../../utils/logto_utils";
 
 export class AuditLogsListPage {
   private readonly header: Locator;
@@ -38,14 +39,41 @@ export class AuditLogsListPage {
     ).toBeVisible();
   }
 
+  async checkMultipleAuditLogs(params: {
+    resourceId: string;
+    eventType: AuditLogEventType;
+    number: number;
+  }) {
+    await expect(
+      await this.page
+        .locator(`tr[data-resource-id="${params.resourceId}"]`)
+        .filter({
+          has: this.page.getByRole("cell", {
+            name: AuditLogEventTitles[params.eventType],
+          }),
+        })
+        .filter({ has: this.page.getByRole("link", { name: "Details" }) }),
+    ).toHaveCount(params.number);
+  }
+
   async filterAuditLog() {}
 
-  async goToDetails(resourceId: string, eventType: AuditLogEventType) {
+  async goToDetails(
+    resourceId: string,
+    eventType: AuditLogEventType,
+    page = this.page,
+  ) {
+    const userId = await getUserId(page);
     const auditLogRow = await this.page
       .locator(`tr[data-resource-id="${resourceId}"]`)
       .filter({
         has: this.page.getByRole("cell", {
           name: AuditLogEventTitles[eventType],
+        }),
+      })
+      .filter({
+        has: this.page.getByRole("cell", {
+          name: userId,
         }),
       });
     const link = auditLogRow.getByRole("link", { name: "Details" });
