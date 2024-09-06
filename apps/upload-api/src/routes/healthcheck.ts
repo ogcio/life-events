66401e34ea6fc76d751e71791e7f8c3ff6887b94
Francesco Maida <francesco.maida@nearform.com>
2024-09-06 15:32:21 +0200

    added dummy getToken

--- apps/upload-api/src/routes/healthcheck.ts
+++ apps/upload-api/src/routes/healthcheck.ts
@@ -1,7 +1,10 @@
 import { FastifyInstance } from "fastify";
+import { getSchedulerSdk } from "../utils/authentication-factory.js";
 
 export default async function healthCheck(app: FastifyInstance) {
   app.get("/health", async () => {
+    const schedulerSdk = await getSchedulerSdk("ogcio");
+
     return { status: "ok" };
   });
 }
