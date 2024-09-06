573cec43391084e697d06a4ae9d89ef175514e22
Francesco Maida <francesco.maida@nearform.com>
2024-09-06 12:56:33 +0200

    chore(scheduler) Implemented build process and optimised dockerfile (#936)

--- apps/scheduler-api/src/config.ts
+++ apps/scheduler-api/src/config.ts
@@ -6,7 +6,6 @@ export const envSchema = {
     "POSTGRES_HOST",
     "POSTGRES_PORT",
     "POSTGRES_DB_NAME",
-    "POSTGRES_DB_NAME_SHARED",
   ],
   properties: {
     POSTGRES_USER: {
@@ -22,9 +21,6 @@ export const envSchema = {
       type: "string",
     },
     POSTGRES_DB_NAME: {
-      type: "string",
-    },
-    POSTGRES_DB_NAME_SHARED: {
       type: "string",
     },
   },
