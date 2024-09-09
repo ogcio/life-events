51ffccc5cc1c2209fc795f616c3ce3038ed66aa0
Francesco Maida <francesco.maida@nearform.com>
2024-09-09 15:43:23 +0200

    Update apps/upload-api/src/routes/healthcheck.ts
    Co-authored-by: SamSalvatico <40636569+SamSalvatico@users.noreply.github.com>

--- apps/upload-api/src/routes/healthcheck.ts
+++ apps/upload-api/src/routes/healthcheck.ts
@@ -1,5 +1,4 @@
 import { FastifyInstance } from "fastify";
-import { getSchedulerSdk } from "../utils/authentication-factory.js";
 
 export default async function healthCheck(app: FastifyInstance) {
   app.get("/health", async () => {
