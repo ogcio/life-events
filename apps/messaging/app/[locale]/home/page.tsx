831c4acab749aad35148236b1ba4add52df6d033
Ludwig Thurfjell <ludwig.thurfjell@gmail.com>
2024-09-10 16:52:06 +0200

    move maintsx and fix params

--- apps/messaging/app/[locale]/home/page.tsx
+++ apps/messaging/app/[locale]/home/page.tsx
@@ -2,18 +2,9 @@ import { Footer, PhaseBanner, Link, Container } from "@govie-react/ds";
 import Header from "../Header";
 import { getTranslations } from "next-intl/server";
 import { getLinks } from "../../utils/messaging";
+import Main from "./Main";
 
-/***
- * There's no real layouts currently available from the govie-ds
- * and we need something placeholder to let main take appropriate size.
- *
- * The body is styled as a flex column
- */
-function Main({ children }: React.PropsWithChildren) {
-  return <main style={{ flex: "auto" }}>{children}</main>;
-}
-
-export default async (params: { locale: string }) => {
+export default async ({ params }: { params: { locale: string } }) => {
   const phaseBannerContent = await getTranslations("FeedbackBanner");
   const environment = String(process.env.ENVIRONMENT);
   const links = getLinks(environment, params.locale);
