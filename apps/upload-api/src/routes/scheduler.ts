8d392efe9246ff6e929321b7c3fbc580b5652139
Francesco Maida <francesco.maida@nearform.com>
2024-09-19 16:42:28 +0200

    scheduler logic

--- apps/upload-api/src/routes/scheduler.ts
+++ apps/upload-api/src/routes/scheduler.ts
@@ -1,5 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { getConfigValue } from "../utils/storeConfig.js";
+import {
+  getExpiredFiles,
+  markFilesAsDeleted,
+} from "./metadata/utils/filesMetadata.js";
+import { DeleteObjectsCommand } from "@aws-sdk/client-s3";
+import { FileMetadataType } from "../types/schemaDefinitions.js";
 
 export default async function schduler(app: FastifyInstance) {
   app.post<{ Body: { schedulerToken: string } }>(
@@ -17,9 +23,123 @@ export default async function schduler(app: FastifyInstance) {
         return { status: "ok" };
       }
 
-      //TODO: handle logic here
+      const now = new Date();
+
+      app.log.info(`Cleanup job running at ${now.toISOString()}`);
+
+      const filesToDeleteQueryResult = await getExpiredFiles(app.pg.pool, now);
+
+      if (!filesToDeleteQueryResult.rows.length) {
+        return { status: "ok" };
+      }
+
+      const filesToDelete = filesToDeleteQueryResult.rows;
+
+      checkLeftoverFiles(app, filesToDelete);
+      checkStaleUndeletedFiles(app, filesToDelete, now);
+
+      const metadataToMarkAsdeleted = await deleteFilesFromStorage(
+        app,
+        filesToDelete,
+      );
+
+      if (!markFilesAsDeleted.length) {
+        return { status: "ok" };
+      }
+
+      try {
+        await markFilesAsDeleted(app.pg.pool, metadataToMarkAsdeleted);
+      } catch (err) {
+        app.log.error(err);
+      }
+
+      //TODO: schedule the new job to run in some time
 
       return { status: "ok" };
     },
   );
 }
+
+/**
+ *
+ * Logs if there are leftover files after deletion
+ *
+ * @param app
+ * @param files
+ */
+const checkLeftoverFiles = (
+  app: FastifyInstance,
+  files: FileMetadataType[],
+) => {
+  const exceedingFiles = files.length - 100;
+  if (exceedingFiles > 0) {
+    app.log.info(`${exceedingFiles} files will be deleted on next iteration`);
+  }
+};
+
+/**
+ * Logs if there are some files that have not been deleted for 3 days
+ */
+const checkStaleUndeletedFiles = (
+  app: FastifyInstance,
+  filesToDelete: FileMetadataType[],
+  now: Date,
+) => {
+  let count = 0;
+  for (const fileToDelete of filesToDelete) {
+    const days = (fileToDelete.scheduledDeletionAt as Date).getDate();
+    if (now.getDate() - days >= 3) {
+      count++;
+    }
+  }
+  if (count > 0) {
+    app.log.info(`${count} files were not deleting for at least 3 days`);
+  }
+};
+
+/**
+ *
+ * Deletes files from storage and returns the list of Ids of metatada
+ * to mark as deleted
+ *
+ * @param app
+ * @param filesToDelete
+ * @returns
+ */
+const deleteFilesFromStorage = async (
+  app: FastifyInstance,
+  filesToDelete: FileMetadataType[],
+) => {
+  const storageKeysToDelete = filesToDelete.map(({ key }) => ({
+    Key: key,
+  }));
+  const metaDataToDelete = new Set<string>(
+    filesToDelete.map(({ id }) => id as string),
+  );
+
+  try {
+    const response = await app.s3Client.client.send(
+      new DeleteObjectsCommand({
+        Bucket: app.s3Client.bucketName,
+        Delete: {
+          Objects: storageKeysToDelete,
+        },
+      }),
+    );
+
+    if (response.Errors) {
+      for (const { Code, Key, Message } of response.Errors) {
+        app.log.error(`${Code} ${Key} ${Message}`);
+
+        // remove from the metadata to remove
+        const fileToDelete = filesToDelete.find(({ key }) => key === Key);
+        metaDataToDelete.delete(fileToDelete?.id as string);
+      }
+    }
+  } catch (err) {
+    app.log.error(err);
+    return [] as string[];
+  }
+
+  return Array.from(metaDataToDelete.keys());
+};
