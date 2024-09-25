6f0bcd6ab871c6d9252c6e3ddd5f651391487413
Francesco Maida <francesco.maida@nearform.com>
2024-09-25 16:33:44 +0200

    chore(explorer-api) Added env var for scheduler org

--- apps/upload-api/src/utils/scheduleCleanupTask.ts
+++ apps/upload-api/src/utils/scheduleCleanupTask.ts
@@ -3,6 +3,10 @@ import { getSchedulerSdk } from "./authentication-factory.js";
 import { getConfigValue, SCHEDULER_TOKEN } from "./storeConfig.js";
 
 const scheduleCleanupTask = async (app: FastifyInstance) => {
+  /**
+   * The org id is hardcoded for now,
+   * we will change this in the future once we have a better m2m management
+   */
   const schedulerSdk = await getSchedulerSdk(
     app.config.ORGANIZATION_ID as string,
   );
