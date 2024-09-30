bbb1821c72c3647fb71fc21aa6af70c51c6b6d70
Ludwig Thufjell <ludwig.thurfjell@gmail.com>
2024-09-30 16:22:35 +0200

    Messaging Inbox - Design system header component (#1011)
    * added design system header component
    
    * corrected flawed is seen logic
    
    * Apply suggestions from code review

--- apps/messaging/app/[locale]/home/[messageId]/page.tsx
+++ apps/messaging/app/[locale]/home/[messageId]/page.tsx
@@ -1,13 +1,13 @@
-
 import { AuthenticationFactory } from "../../../utils/authentication-factory";
 import { getTranslations } from "next-intl/server";
 import { allUrl, unreadUrl } from "../utils";
 import { notFound } from "next/navigation";
 import { Heading, Paragraph } from "@govie-ds/react";
+import React from "react";
 
 type Props = {
   params: { messageId: string };
-  searchParams?:{tab:"all" | "unread"}
+  searchParams?: { tab: "all" | "unread" };
 };
 
 export default async function inboxMessage(props: Props) {
@@ -21,13 +21,22 @@ export default async function inboxMessage(props: Props) {
 
   // We'll mark as see if we open the page.
   await messagingSdk.seeMessage(props.params.messageId);
-  
+
   return (
     <>
       <Heading>{message.data?.subject}</Heading>
-      <Paragraph whitespace="pre-wrap" size="sm">{message.data?.excerpt}</Paragraph>
-      <Paragraph whitespace="pre-wrap" size="md">{message.data?.plainText}</Paragraph>
-      <a className="govie-back-link" href={props.searchParams?.tab === "unread" ? unreadUrl.href : allUrl.href}>
+      <Paragraph whitespace="pre-wrap" size="sm">
+        {message.data?.excerpt}
+      </Paragraph>
+      <Paragraph whitespace="pre-wrap" size="md">
+        {message.data?.plainText}
+      </Paragraph>
+      <a
+        className="govie-back-link"
+        href={
+          props.searchParams?.tab === "unread" ? unreadUrl.href : allUrl.href
+        }
+      >
         {tPanel("backLink")}
       </a>
     </>
