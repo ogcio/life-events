d919927b0515f7d20c4146e87e3af0ebda2b906c
Ludwig Thurfjell <ludwig.thurfjell@gmail.com>
2024-09-30 14:27:17 +0200

    added design system header component

--- apps/messaging/app/[locale]/home/page.tsx
+++ apps/messaging/app/[locale]/home/page.tsx
@@ -1,8 +1,22 @@
-import { Link, Heading, Paragraph, Tabs, TabItem, Table, TableHeader, TableBody, TableRow, TableHead, TableData, Icon } from "@govie-ds/react";
+import {
+  Link,
+  Heading,
+  Paragraph,
+  Tabs,
+  TabItem,
+  Table,
+  TableHeader,
+  TableBody,
+  TableRow,
+  TableHead,
+  TableData,
+  Icon,
+} from "@govie-ds/react";
 import { getTranslations } from "next-intl/server";
 import { AuthenticationFactory } from "../../utils/authentication-factory";
 import dayjs from "dayjs";
 import { allUrl, baseUrl, unreadUrl } from "./utils";
+import React from "react";
 
 export default async (props: { searchParams?: { tab?: string } }) => {
   const tHome = await getTranslations("Home");
@@ -10,8 +24,10 @@ export default async (props: { searchParams?: { tab?: string } }) => {
   const messagingSdk = await AuthenticationFactory.getMessagingClient();
   const userInfo = await AuthenticationFactory.getInstance().getUser();
 
-  const isSeenQuery = props.searchParams?.tab === "unread" ? false : undefined;
-
+  let isSeenQuery: boolean | undefined = false;
+  if (props.searchParams?.tab) {
+    isSeenQuery = props.searchParams?.tab === "all";
+  }
   const messages = await messagingSdk.getMessagesForUser(userInfo.id, {
     offset: 0,
     limit: 100,
@@ -24,19 +40,15 @@ export default async (props: { searchParams?: { tab?: string } }) => {
 
       <Tabs>
         <TabItem
-        value="unread"
-          checked={
-            !props.searchParams?.tab || props.searchParams?.tab === "unread"
-          }
+          value="unread"
+          checked={Boolean(!isSeenQuery)}
           href={unreadUrl.href}
         >
           {tHome("unread")}
         </TabItem>
         <TabItem
           value="all"
-          checked={Boolean(
-            props.searchParams?.tab && props.searchParams?.tab === "all",
-          )}
+          checked={Boolean(Boolean(isSeenQuery))}
           href={allUrl.href}
         >
           {tHome("all")}
@@ -44,7 +56,7 @@ export default async (props: { searchParams?: { tab?: string } }) => {
       </Tabs>
 
       <Table>
-        <TableHead>          
+        <TableHead>
           <TableRow>
             <TableHeader>{tHome("date")}</TableHeader>
             <TableHeader>{tHome("details")}</TableHeader>
@@ -54,27 +66,38 @@ export default async (props: { searchParams?: { tab?: string } }) => {
         <TableBody>
           {messages.data?.map((message) => (
             <TableRow key={message.id}>
-              <TableData>{dayjs(message.createdAt).format("D MMM YYYY")}</TableData>
+              <TableData>
+                {dayjs(message.createdAt).format("D MMM YYYY")}
+              </TableData>
               <TableData>
                 <Link
                   href={((messageId) => {
-                    console.log({isSeenQuery})
                     const url = new URL(baseUrl);
-                    url.pathname = `/home/${messageId}`;                    
-                    url.searchParams.append("tab", isSeenQuery === undefined ? "all" : "unread")                    
+                    url.pathname = `/home/${messageId}`;
+                    url.searchParams.append(
+                      "tab",
+                      isSeenQuery ? "all" : "unread",
+                    );
                     return url.href;
                   })(message.id)}
                 >
                   {message.subject}
                 </Link>
               </TableData>
-              <TableData><Icon icon="attach_file" color={message.attachmentsCount ? "default" :"disabled"}/></TableData>
+              <TableData>
+                <Icon
+                  icon="attach_file"
+                  color={message.attachmentsCount ? "default" : "disabled"}
+                />
+              </TableData>
             </TableRow>
           ))}
         </TableBody>
       </Table>
 
-      {!messages.data?.length && <Paragraph align="center">{tHome("noMessages")}</Paragraph>}
+      {!messages.data?.length && (
+        <Paragraph align="center">{tHome("noMessages")}</Paragraph>
+      )}
     </>
   );
 };
