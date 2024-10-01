45436c41d8ca44ab445680debee6544631d9d3b8
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-10-01 16:25:21 +0200

    fixed forbidden usage

--- apps/messaging-api/src/utils/error-utils.ts
+++ apps/messaging-api/src/utils/error-utils.ts
@@ -34,7 +34,7 @@ export const ensureUserIsOrganisationMember = (
   user: { organizationId?: string } | undefined,
 ): string => {
   if (!user?.organizationId) {
-    throw httpErrors.unauthorized(
+    throw httpErrors.forbidden(
       "You have to be part of an organisation to invoke this endpoint",
     );
   }
