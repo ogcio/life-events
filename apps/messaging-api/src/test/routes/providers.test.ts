5ed1d72fba5d6c12f247af31415de31120229e72
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-10-03 14:56:11 +0200

    fixed import

--- apps/messaging-api/src/test/routes/providers.test.ts
+++ apps/messaging-api/src/test/routes/providers.test.ts
@@ -12,6 +12,7 @@ t.test("messaging - /api/v1/providers schema", async (t) => {
               userId: "userId",
               accessToken: "accessToken",
               organizationId: "organisationId",
+              isM2MApplication: false,
             };
           });
         }),
