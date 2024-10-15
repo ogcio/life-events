ac64d43e41fc4a454942e1d6400ab0eabe635623
Sara Zanellato <sara.zanellato@nearform.com>
2024-10-15 14:49:59 +0200

    feat: get amount from token for stripe

--- apps/payments/app/[locale]/(hosted)/paymentRequest/stripe/page.tsx
+++ apps/payments/app/[locale]/(hosted)/paymentRequest/stripe/page.tsx
@@ -5,8 +5,13 @@ import { AbstractIntlMessages, NextIntlClientProvider } from "next-intl";
 import { redirect, RedirectType } from "next/navigation";
 import { errorHandler } from "../../../../utils";
 import { AuthenticationFactory } from "../../../../../libraries/authentication-factory";
+import { getAmount } from "../utils";
 
-async function getPaymentDetails(paymentId: string, amount?: string) {
+async function getPaymentDetails(
+  paymentId: string,
+  token?: string,
+  customAmount?: string,
+) {
   const paymentsApi = await AuthenticationFactory.getPaymentsClient();
   const { data: details, error } =
     await paymentsApi.getPaymentRequestPublicInfo(paymentId);
@@ -23,15 +28,14 @@ async function getPaymentDetails(paymentId: string, amount?: string) {
 
   if (!provider) return undefined;
 
+  const amount = await getAmount({ customAmount, token, prDetails: details });
+
   return {
     ...details,
     providerId: provider.id,
     providerName: provider.name,
     providerData: provider.data,
-    amount:
-      details.allowAmountOverride && amount
-        ? parseFloat(amount)
-        : details.amount,
+    amount,
   };
 }
 
@@ -41,9 +45,10 @@ export default async function Card(props: {
     | {
         paymentId: string;
         integrationRef: string;
-        amount?: string;
+        token?: string;
         submissionId?: string;
         journeyId?: string;
+        customAmount?: string;
       }
     | undefined;
 }) {
@@ -68,7 +73,8 @@ export default async function Card(props: {
 
   const paymentDetails = await getPaymentDetails(
     paymentId,
-    props.searchParams.amount,
+    props.searchParams.token,
+    props.searchParams.customAmount,
   );
 
   if (!paymentDetails) {
