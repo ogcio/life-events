5229a53defd26471827359ab6ae46b4531b61f77
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-10-15 16:24:07 +0200

    Revert "Messaging: log error while getting token (#1055)"
    This reverts commit f73a362a6b2e6003d8026615a7257f9d53a8fa06.

--- apps/messaging/app/utils/authentication-factory.ts
+++ apps/messaging/app/utils/authentication-factory.ts
@@ -2,7 +2,6 @@ import { BaseAuthenticationContext } from "auth/base-authentication-context";
 import { getAuthenticationContextConfig } from "./logto-config";
 import { Messaging } from "building-blocks-sdk";
 import { headers } from "next/headers";
-import { getCommonLogger } from "nextjs-logging-wrapper";
 
 export class AuthenticationFactory {
   static getInstance(): BaseAuthenticationContext {
@@ -13,51 +12,18 @@ export class AuthenticationFactory {
     // call a route handler that retrieves the cached token
     // we need to forward the cookie header or the request won't be authenticated
     const cookieHeader = headers().get("cookie") as string;
-    let res: Response | null = null;
-    try {
-      res = await fetch(
-        new URL(
-          "/api/token",
-          process.env.NEXT_PUBLIC_MESSAGING_SERVICE_ENTRY_POINT as string,
-        ),
-        { headers: { cookie: cookieHeader } },
-      );
-      const { token } = await res.json();
-      return token;
-    } catch (e) {
-      const logger = getCommonLogger();
-      logger.fatal(e, "Error while getting token");
-      if (res) {
-        logger.fatal(
-          {
-            response_body:
-              res.body === null ? "IS NULL" : this.streamToString(res.body),
-          },
-          "Get token response body",
-        );
-      }
-      throw e;
-    }
+
+    const res = await fetch(
+      new URL(
+        "/api/token",
+        process.env.NEXT_PUBLIC_MESSAGING_SERVICE_ENTRY_POINT as string,
+      ),
+      { headers: { cookie: cookieHeader } },
+    );
+    const { token } = await res.json();
+    return token;
   }
 
-  static async streamToString(stream: ReadableStream<Uint8Array>) {
-    const reader = stream.getReader();
-    const textDecoder = new TextDecoder();
-    let result = "";
-
-    async function read() {
-      const { done, value } = await reader.read();
-
-      if (done) {
-        return result;
-      }
-
-      result += textDecoder.decode(value, { stream: true });
-      return read();
-    }
-
-    return read();
-  }
   static async getMessagingClient(): Promise<Messaging> {
     const token = await this.getToken();
     return new Messaging(token);
