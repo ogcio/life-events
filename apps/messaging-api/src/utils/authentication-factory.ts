68e1d44c1a412ac142cdf6345ee37d898d270862
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-10-15 08:38:58 +0200

    updated messaging bb

--- apps/messaging-api/src/utils/authentication-factory.ts
+++ apps/messaging-api/src/utils/authentication-factory.ts
@@ -1,6 +1,15 @@
-import { getAccessToken, getOrganizationToken } from "api-auth";
-import { Profile, Scheduler, Upload } from "building-blocks-sdk";
+import getBuildingBlockSDK, {
+  BuildingBlocksSDK,
+  getM2MTokenFn,
+} from "@ogcio/building-blocks-sdk";
 import { httpErrors } from "@fastify/sensible";
+
+const profileBackendUrl = process.env.PROFILE_BACKEND_URL?.endsWith("/")
+  ? process.env.PROFILE_BACKEND_URL
+  : `${process.env.PROFILE_BACKEND_URL}/`;
+
+const sdkPerOrganisation: { [organizationId: string]: BuildingBlocksSDK } = {};
+let sdkPerCitizen: BuildingBlocksSDK | undefined = undefined;
 
 const getBaseProfileConfig = (): {
   logtoOidcEndpoint: string;
@@ -12,49 +21,84 @@ const getBaseProfileConfig = (): {
   applicationSecret: process.env.LOGTO_M2M_PROFILE_APP_SECRET ?? "",
 });
 
-const getOrganizationProfileToken = (organizationId: string): Promise<string> =>
-  getOrganizationToken({
-    ...getBaseProfileConfig(),
-    scopes: ["profile:user:read"],
-    organizationId,
-  });
-
-const getCitizenProfileToken = (): Promise<string> =>
-  getAccessToken({
-    ...getBaseProfileConfig(),
-    scopes: ["profile:user:read"],
-    resource: process.env.PROFILE_BACKEND_URL?.endsWith("/")
-      ? process.env.PROFILE_BACKEND_URL
-      : `${process.env.PROFILE_BACKEND_URL}/`,
-  });
-
-export const getProfileSdk = async (
-  organizationId?: string,
-): Promise<Profile> => {
-  const token = await (organizationId
-    ? getOrganizationProfileToken(organizationId)
-    : getCitizenProfileToken());
-
-  return new Profile(token);
+export const getProfileSdk = async (organizationId?: string) => {
+  return loadBuildingBlocksSdk(organizationId).profile;
 };
 
-const getOrganizationSchedulerToken = (
-  organizationId: string,
-): Promise<string> =>
-  getOrganizationToken({
-    logtoOidcEndpoint: process.env.LOGTO_OIDC_ENDPOINT ?? "",
-    applicationId: process.env.LOGTO_M2M_SCHEDULER_APP_ID ?? "",
-    applicationSecret: process.env.LOGTO_M2M_SCHEDULER_APP_SECRET ?? "",
-    scopes: ["scheduler:jobs:write"],
-    organizationId,
-  });
+export const getSchedulerSdk = async (organizationId: string) => {
+  return loadBuildingBlocksSdk(organizationId).scheduler;
+};
 
-export const getSchedulerSdk = async (
-  organizationId: string,
-): Promise<Scheduler> => {
-  const token = await getOrganizationSchedulerToken(organizationId);
+const servicesConfiguration = {
+  scheduler: {
+    baseUrl: process.env.SCHEDULER_BACKEND_URL,
+  },
+  profile: {
+    baseUrl: process.env.PROFILE_BACKEND_URL,
+  },
+  upload: {
+    baseUrl: process.env.UPLOAD_BACKEND_URL,
+  },
+};
 
-  return new Scheduler(token);
+const loadBuildingBlocksSdk = (organizationId?: string): BuildingBlocksSDK => {
+  if (!organizationId) {
+    if (!sdkPerCitizen) {
+      sdkPerCitizen = getBuildingBlockSDK({
+        services: servicesConfiguration,
+        getTokenFn: getM2MTokenFn({
+          services: {
+            profile: {
+              getAccessTokenParams: {
+                resource: profileBackendUrl,
+                scopes: ["profile:user.self:read"],
+                ...getBaseProfileConfig(),
+              },
+            },
+          },
+        }),
+      });
+    }
+    return sdkPerCitizen;
+  }
+
+  if (!sdkPerOrganisation[organizationId]) {
+    sdkPerOrganisation[organizationId] = getBuildingBlockSDK({
+      services: servicesConfiguration,
+      getTokenFn: getM2MTokenFn({
+        services: {
+          profile: {
+            getOrganizationTokenParams: {
+              scopes: ["profile:user:read"],
+              organizationId,
+              ...getBaseProfileConfig(),
+            },
+          },
+          scheduler: {
+            getOrganizationTokenParams: {
+              logtoOidcEndpoint: process.env.LOGTO_OIDC_ENDPOINT ?? "",
+              applicationId: process.env.LOGTO_M2M_SCHEDULER_APP_ID ?? "",
+              applicationSecret:
+                process.env.LOGTO_M2M_SCHEDULER_APP_SECRET ?? "",
+              scopes: ["scheduler:jobs:write"],
+              organizationId,
+            },
+          },
+          upload: {
+            getOrganizationTokenParams: {
+              logtoOidcEndpoint: process.env.LOGTO_OIDC_ENDPOINT ?? "",
+              applicationId: process.env.LOGTO_M2M_UPLOADER_APP_ID ?? "",
+              applicationSecret:
+                process.env.LOGTO_M2M_UPLOADER_APP_SECRET ?? "",
+              scopes: ["upload:file:*"],
+              organizationId,
+            },
+          },
+        },
+      }),
+    });
+  }
+  return sdkPerOrganisation[organizationId];
 };
 
 export const ensureUserIdIsSet = (request: {
@@ -77,17 +121,6 @@ export const ensureOrganizationIdIsSet = (request: {
   throw httpErrors.forbidden("Organization id is not set");
 };
 
-const getOrganizationUploadToken = (organizationId: string): Promise<string> =>
-  getOrganizationToken({
-    logtoOidcEndpoint: process.env.LOGTO_OIDC_ENDPOINT ?? "",
-    applicationId: process.env.LOGTO_M2M_UPLOADER_APP_ID ?? "",
-    applicationSecret: process.env.LOGTO_M2M_UPLOADER_APP_SECRET ?? "",
-    scopes: ["upload:file:*"],
-    organizationId,
-  });
-
-export const getUploadSdk = async (organizationId: string): Promise<Upload> => {
-  const token = await getOrganizationUploadToken(organizationId);
-
-  return new Upload(token);
+export const getUploadSdk = async (organizationId: string) => {
+  return loadBuildingBlocksSdk(organizationId).upload;
 };
