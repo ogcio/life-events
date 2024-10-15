5f245ccc047d7c1955acd6276710e143f7c953b1
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-10-15 16:23:58 +0200

    Revert "use clone (#1056)"
    This reverts commit f4314d5b7acf15abdf3250d54e44649199a721b9.

--- apps/messaging/app/utils/authentication-factory.ts
+++ apps/messaging/app/utils/authentication-factory.ts
@@ -13,28 +13,25 @@ export class AuthenticationFactory {
     // call a route handler that retrieves the cached token
     // we need to forward the cookie header or the request won't be authenticated
     const cookieHeader = headers().get("cookie") as string;
-    let responseClone: Response | null = null;
+    let res: Response | null = null;
     try {
-      const res = await fetch(
+      res = await fetch(
         new URL(
           "/api/token",
           process.env.NEXT_PUBLIC_MESSAGING_SERVICE_ENTRY_POINT as string,
         ),
         { headers: { cookie: cookieHeader } },
       );
-      responseClone = res.clone();
       const { token } = await res.json();
       return token;
     } catch (e) {
       const logger = getCommonLogger();
       logger.fatal(e, "Error while getting token");
-      if (responseClone) {
+      if (res) {
         logger.fatal(
           {
             response_body:
-              responseClone.body === null
-                ? "IS NULL"
-                : await this.streamToString(responseClone.body),
+              res.body === null ? "IS NULL" : this.streamToString(res.body),
           },
           "Get token response body",
         );
