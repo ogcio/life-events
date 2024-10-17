8766e86edc36899013020465e55eca09975b9bc7
SamSalvatico <40636569+SamSalvatico@users.noreply.github.com>
2024-10-17 10:55:21 +0200

    chore: copied payments middleware to upload and profile (#1069)

--- apps/upload/middleware.ts
+++ apps/upload/middleware.ts
@@ -1,21 +1,18 @@
 import createMiddleware from "next-intl/middleware";
-import { NextRequest } from "next/server";
+import type { NextRequest } from "next/server";
 const locales = ["en", "ga"];
 
-export default function (request: NextRequest) {
-  const nextResponse = createMiddleware({ locales, defaultLocale: "en" })(
-    request,
-  );
+export default async function (request: NextRequest) {
+  const nextResponse = createMiddleware({
+    locales,
+    defaultLocale: "en",
+  })(request);
 
-  nextResponse.headers.append("x-pathname", request.nextUrl.pathname);
-  nextResponse.headers.append(
-    "x-searchParams",
-    request.nextUrl.searchParams.toString(),
-  );
+  nextResponse.headers.append("x-url", request.url);
 
   return nextResponse;
 }
 
 export const config = {
-  matcher: ["/((?!static|api|health|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!static|health|api|_next/static|_next/image|favicon.ico).*)"],
 };
