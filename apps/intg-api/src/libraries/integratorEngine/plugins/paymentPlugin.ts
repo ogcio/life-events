bd87840caafb76b45b593cb67c08fa6033326079
alfonsograziano <alfonsohack0019@gmail.com>
2024-10-23 12:37:19 +0200

    wip

--- apps/intg-api/src/libraries/integratorEngine/plugins/paymentPlugin.ts
+++ apps/intg-api/src/libraries/integratorEngine/plugins/paymentPlugin.ts
@@ -1,5 +1,7 @@
 import { PaymentStepDataDO } from "../../../plugins/entities/journeySteps/types";
+import { keyAlias } from "../../../utils/kms";
 import { IIntegratorPlugin, IntegratorProps } from "./basePlugin";
+import { createSignedJWT } from "api-auth";
 
 export class PaymentPlugin implements IIntegratorPlugin {
   public async execute(
@@ -7,8 +9,34 @@ export class PaymentPlugin implements IIntegratorPlugin {
     props: IntegratorProps,
   ): Promise<{ url: string }> {
     const url = new URL(stepData.url);
+    //TODO: Don't pass these info in the URL params, but just take them from the Token
     url.searchParams.set("journeyId", props.journeyId);
     url.searchParams.set("runId", props.runId);
+
+    const redirectUrl = new URL(
+      `/journey/${props.journeyId}/callback`,
+      props.host,
+    );
+    url.searchParams.set("runId", props.runId);
+
+    //TODO: take this dinamically, for now is just to test that it work
+    const amount = 100;
+
+    const jwt = await createSignedJWT(
+      {
+        runId: props.runId,
+        journeyId: props.journeyId,
+        amount,
+        redirectUrl,
+      },
+      keyAlias,
+      {
+        audience: "payments-api",
+        issuer: "integrator-api",
+      },
+    );
+
+    url.searchParams.set("token", jwt);
 
     return { url: url.href };
   }
