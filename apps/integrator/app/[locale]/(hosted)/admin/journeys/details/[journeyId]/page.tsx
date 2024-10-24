79e65af14b1eca8275426451db389e53db1ffd0e
Norbert Nagy <norbert.nagy@nearform.com>
2024-10-24 16:16:04 +0300

    feat(22610): journey details page

--- apps/integrator/app/[locale]/(hosted)/admin/journeys/details/[journeyId]/page.tsx
+++ apps/integrator/app/[locale]/(hosted)/admin/journeys/details/[journeyId]/page.tsx
@@ -3,14 +3,9 @@ import { notFound } from "next/navigation";
 import Link from "next/link";
 import { AuthenticationFactory } from "../../../../../../../libraries/authentication-factory";
 import { PageWrapper } from "../../../../PageWrapper";
-import { Journey } from "../../../../../../../libraries/journeyEditor/types";
-import { loadJourneyById } from "../../../../../../../libraries/journeyEditor/queries";
-import { pgpool } from "../../../../../../dbConnection";
-import advancedFormat from "dayjs/plugin/advancedFormat";
 import dayjs from "dayjs";
 import { generateJourneyLink } from "../../../../../../utils/journey";
-import { JourneyEditor } from "../../../../../../../libraries/journeyEditor";
-import journeyDefaultFlow from "../../../../../../../libraries/journeyEditor/journeyStepFlow";
+import { errorHandler } from "../../../../../../utils/errorHandler";
 
 type Props = {
   params: {
@@ -19,31 +14,7 @@ type Props = {
   };
 };
 
-const loadJourney = async (journeyId: string): Promise<Journey> => {
-  "use server";
-
-  const { organization } =
-    await AuthenticationFactory.getInstance().getContext();
-
-  if (!organization) {
-    throw new Error("Unauthorized!");
-  }
-
-  const result = await loadJourneyById(pgpool, {
-    journeyId,
-    organizationId: organization.id,
-  });
-
-  if (result.rowCount !== 1) {
-    return notFound();
-  }
-
-  return result.rows[0];
-};
-
 export default async ({ params: { locale, journeyId } }: Props) => {
-  dayjs.extend(advancedFormat);
-
   const t = await getTranslations("Journeys.details");
   const tGeneral = await getTranslations("General");
 
@@ -54,81 +25,103 @@ export default async ({ params: { locale, journeyId } }: Props) => {
     return notFound();
   }
 
-  const defaultOrgId = await context.getSelectedOrganization();
-  const journey = await loadJourney(journeyId);
-  const editor = new JourneyEditor(journey, journeyDefaultFlow);
-  const steps = editor.getStepsInfo();
+  const client = await AuthenticationFactory.getIntegratorClient();
+  const { data: journey, error } = await client.getJourneyById(journeyId);
+
+  const errors = errorHandler(error);
 
   return (
     <PageWrapper locale={locale}>
       <div className="govie-width-container" style={{ width: "100%" }}>
         <div style={{ width: "100%" }}>
-          <div>
-            <h1
-              className="govie-heading-m"
-              style={{
-                float: "left",
-              }}
-            >
-              {journey.title}
-            </h1>
+          <div
+            className="govie-width-container"
+            style={{ width: "100%", fontSize: "16px" }}
+          >
             <Link
               className="govie-link"
-              href={`/${locale}/admin/journeys/configure/${journey.id}`}
-              style={{
-                fontSize: "22px",
-                fontWeight: "700",
-                float: "right",
-                lineHeight: "46px",
-              }}
+              href={`/${locale}/admin/journeys/list`}
             >
-              {tGeneral("edit")}
+              {`< ${tGeneral("back")}`}
             </Link>
           </div>
 
-          <dl className="govie-summary-list">
-            <div className="govie-summary-list__row">
-              <dt className="govie-summary-list__key">{t("status")}</dt>
-              <dt className="govie-summary-list__value">
-                {tGeneral(journey.status)}
-              </dt>
-            </div>
-            <div className="govie-summary-list__row">
-              <dt className="govie-summary-list__key">{t("created")}</dt>
-              <dt className="govie-summary-list__value">
-                {dayjs(journey.createdAt).format("Do MMM YYYY")}
-              </dt>
-            </div>
-            <div className="govie-summary-list__row">
-              <dt className="govie-summary-list__key">{t("createdBy")}</dt>
-              <dt className="govie-summary-list__value">
-                {/* {await getUserNameById(journey.userId, defaultOrgId)} */}
-              </dt>
-            </div>
-            <div className="govie-summary-list__row">
-              <dt className="govie-summary-list__key">{t("journeyLink")}</dt>
-              <dt className="govie-summary-list__value">
-                <Link
-                  className="govie-link"
-                  href={generateJourneyLink(journeyId)}
-                >
-                  {generateJourneyLink(journeyId)}
-                </Link>
-              </dt>
-            </div>
-          </dl>
-
-          <h2 className="govie-heading-m">{t("steps")}</h2>
-
-          <dl className="govie-summary-list">
-            <div className="govie-summary-list__row">
-              <dt className="govie-summary-list__key">{t("buildingBlock")}</dt>
-              <dt className="govie-summary-list__value">
-                <strong>{t("description")}</strong>
-              </dt>
+          <section
+            style={{
+              margin: "1rem 0",
+              flex: 1,
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <div>
+              <h1
+                className="govie-heading-m"
+                style={{
+                  float: "left",
+                }}
+              >
+                {journey?.data.title}
+              </h1>
+              <Link
+                className="govie-link"
+                href={`/${locale}/admin/journeys/configure/${journey?.data.id}`}
+                style={{
+                  fontSize: "22px",
+                  fontWeight: "700",
+                  float: "right",
+                  lineHeight: "46px",
+                }}
+              >
+                {tGeneral("edit")}
+              </Link>
             </div>
 
-            {steps.map((step) => {
+            <dl className="govie-summary-list">
+              <div className="govie-summary-list__row">
+                <dt className="govie-summary-list__key">{t("status")}</dt>
+                <dt className="govie-summary-list__value">
+                  {tGeneral(journey?.data.status)}
+                </dt>
+              </div>
+              <div className="govie-summary-list__row">
+                <dt className="govie-summary-list__key">{t("created")}</dt>
+                <dt className="govie-summary-list__value">
+                  {dayjs(journey?.data.createdAt).format("Do MMM YYYY")}
+                </dt>
+              </div>
+              <div className="govie-summary-list__row">
+                <dt className="govie-summary-list__key">{t("createdBy")}</dt>
+                <dt className="govie-summary-list__value">
+                  {/* {await getUserNameById(journey.userId, defaultOrgId)} */}
+                </dt>
+              </div>
+              <div className="govie-summary-list__row">
+                <dt className="govie-summary-list__key">{t("journeyLink")}</dt>
+                <dt className="govie-summary-list__value">
+                  <Link
+                    className="govie-link"
+                    href={generateJourneyLink(journeyId)}
+                  >
+                    {generateJourneyLink(journeyId)}
+                  </Link>
+                </dt>
+              </div>
+            </dl>
+
+            <h2 className="govie-heading-m">{t("steps")}</h2>
+
+            <dl className="govie-summary-list">
+              <div className="govie-summary-list__row">
+                <dt className="govie-summary-list__key">
+                  {t("buildingBlock")}
+                </dt>
+                <dt className="govie-summary-list__value">
+                  <strong>{t("description")}</strong>
+                </dt>
+              </div>
+
+              {/* {steps.map((step) => {
               return (
                 <div className="govie-summary-list__row">
                   <dt
@@ -142,12 +135,9 @@ export default async ({ params: { locale, journeyId } }: Props) => {
                   </dt>
                 </div>
               );
-            })}
-          </dl>
-
-          <Link className="govie-link" href={`/${locale}/admin/journeys/list`}>
-            {tGeneral("backToExistingJourneys")}
-          </Link>
+            })} */}
+            </dl>
+          </section>
         </div>
       </div>
     </PageWrapper>
