56f6297168403a1074803473e805c6d2713b3569
dlosinski-nf <david.losinski@nearform.com>
2024-10-24 10:55:35 +0100

    update non-functional tests results location

--- apps/web/perf/lighthouse/runLighthouse.mjs
+++ apps/web/perf/lighthouse/runLighthouse.mjs
@@ -25,13 +25,13 @@ export async function runLighthouseForURL (pageURL, opts, reportName) {
       SEO: 0.90
     }
 
-    fs.writeFile('performance_test_reports/' + reportNameForFile + '.html', html, (err) => {
+    fs.writeFile('test_reports/perf/' + reportNameForFile + '-lighthouse.html', html, (err) => {
       if (err) {
         console.error(err)
       }
     })
 
-    fs.writeFile('performance_test_reports/' + reportNameForFile + '.json', json, (err) => {
+    fs.writeFile('test_reports/perf/' + reportNameForFile + '-lighthouse.json', json, (err) => {
       if (err) {
         console.error(err)
       }
